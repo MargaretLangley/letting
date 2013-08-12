b62b0f7b4ad0c9ca53889ae56760ffdf17357eda
Richard Wigley <Richard.Wigley@gmail.com>
2013-08-12 22:10:52 +0100

    FEATURE Import can override headers

--- lib/import/import.rb
+++ lib/import/import.rb
@@ -5,18 +5,22 @@ module DB
   class Import
     attr_reader :location
     attr_reader :drop_rows
+    attr_reader :headers
 
-    def initialize location, drop_rows
+    def initialize location, drop_rows, headers
       @location = location
       @drop_rows = drop_rows
+      @headers = headers
     end
 
     def self.csv_table filename, args = {}
-      new(args.fetch(:location, 'import_data'), args.fetch(:drop_rows,0)).file_to_table filename
+      new(args.fetch(:location, 'import_data'),
+          args.fetch(:drop_rows,0),
+          args.fetch(:headers, true)).file_to_table filename
     end
 
     def file_to_table filename
-      CSV.open( get_file(filename), headers: true, header_converters: :symbol, converters: lambda {|f| f ? f.strip : nil}).read.drop(drop_rows)
+      CSV.open( get_file(filename), headers: headers, header_converters: :symbol, converters: lambda {|f| f ? f.strip : nil}).read.drop(drop_rows)
     end
 
     private
--- spec/lib/import/import_spec.rb
+++ spec/lib/import/import_spec.rb
@@ -3,12 +3,6 @@ require_relative '../../../lib/import/import'
 module DB
   describe Import do
 
-    context 'location' do
-      it 'can be set during initialization' do
-        expect(Import.new('spec/fixtures/import_data', 0).location).to eq 'spec/fixtures/import_data'
-      end
-    end
-
     context 'csv_table' do
       it 'Expected error if file unknown' do
         expect { Import.csv_table 'client' }.to raise_error Errno::ENOENT
@@ -20,12 +14,30 @@ module DB
       end
     end
 
-    context 'drop rows' do
-      it 'can drop a row' do
-        output = Import.csv_table('open_test', location: 'spec/fixtures/import_data', drop_rows: 1)
-        expect(output.length).to eq 1
+    context 'arguments' do
+      context ':location' do
+        it 'can be set during initialization' do
+          expect(Import.new('spec/fixtures/import_data', 0, true).location).to eq 'spec/fixtures/import_data'
+        end
+      end
+
+
+      context ':drop_rows' do
+        it 'can drop a row' do
+          output = Import.csv_table('open_test', location: 'spec/fixtures/import_data', drop_rows: 1)
+          expect(output.length).to eq 1
+        end
+      end
+
+      context ':headers' do
+        it 'overrides headers if required' do
+          output = Import.csv_table('open_test', header: ['one', 'line'], location: 'spec/fixtures/import_data')
+          output.each do |row|
+            expect(row[:one]).to_not be_nil
+            expect(row[:line]).to_not be_nil
+          end
+        end
       end
     end
-
   end
 end
