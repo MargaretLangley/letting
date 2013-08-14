b2154e07b3be9f9b6458d0b9d23d3cdc71b321ac
Richard Wigley <Richard.Wigley@gmail.com>
2013-08-14 22:28:01 +0100

    FEATURE A valid house can have either a House Name or a Road No.

--- app/models/house_address.rb
+++ app/models/house_address.rb
@@ -1,3 +1,4 @@
 class HouseAddress < Address
-  validates :road_no, presence: true
+  validates :road_no, presence: true, unless: :house_name?
+  validates :house_name, presence: true, unless: :road_no?
 end
--- spec/models/house_address_spec.rb
+++ spec/models/house_address_spec.rb
@@ -7,7 +7,15 @@ let(:address) { HouseAddress.new address_attributes addressable_id: 1, type: 'Ho
   it ('valid') { expect(address).to be_valid }
 
   context 'validations presence' do
-    it('#road_no') { address.road_no =nil; expect(address).not_to be_valid }
+    it('#road_no')    { address.road_no =nil; expect(address).to be_valid }
+    it('#house_name') { address.house_name =nil; expect(address).to be_valid }
+
+    it('#road_no & #house_name') do
+      address.road_no =nil
+      address.house_name =nil
+      expect(address).to_not be_valid
+    end
+
   end
 
 end
