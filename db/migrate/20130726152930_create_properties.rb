a889fca87b1da5016f2f1538fe28aab7c5984f3d
Richard Wigley <Richard.Wigley@gmail.com>
2013-08-17 13:11:29 +0100

    CHANGE Removed block_id from property as it wasn't being used

--- db/migrate/20130726152930_create_properties.rb
+++ db/migrate/20130726152930_create_properties.rb
@@ -2,12 +2,10 @@ class CreateProperties < ActiveRecord::Migration
   def change
     create_table :properties do |t|
       t.integer :human_id
-      t.integer :block_id
       t.integer :client_id
       t.timestamps
     end
 
-    add_index :properties, :block_id
     add_index :properties, :client_id
   end
 end
