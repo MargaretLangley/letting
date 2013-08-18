edeeaf56805fedbe5687373222d0cc663ad732f7
Richard Wigley <Richard.Wigley@gmail.com>
2013-08-18 11:58:31 +0100

    FEATURE Added index onto foreign key

--- db/migrate/20130729081107_create_billing_profiles.rb
+++ db/migrate/20130729081107_create_billing_profiles.rb
@@ -2,9 +2,10 @@ class CreateBillingProfiles < ActiveRecord::Migration
   def change
     create_table :billing_profiles do |t|
       t.boolean :use_profile, null: false, default: false
-      t.references :property, null: false
+      t.integer :property_id, null: false
       t.timestamps
     end
+    add_index :billing_profiles, :property_id
   end
 end
 
