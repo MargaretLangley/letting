a4683071fda0736d3d066e6e19b96d6c90103e12
Margaret <margaret.b.langley@gmail.com>
2013-08-22 00:43:37 +0100

    FEATURE District visible/hidden

--- app/assets/javascripts/change_element_display.js
+++ app/assets/javascripts/change_element_display.js
@@ -11,6 +11,22 @@ function setBlockDisplay(elId,fieldset)
   }
 }
 
+function setFieldDisplay(elId)
+{
+  if (document.getElementById(elId).value.length > 0)
+  {
+    document.getElementById(elId).style.visibility="visible";
+  }
+  else
+  {
+    document.getElementById(elId).style.visibility="hidden";
+  }
+}
+
+function changeFieldDisplay(elId)
+{
+  document.getElementById(elId).style.visibility="visible";
+}
 
 function toggleFieldDisplay(fieldset,buttonId)
 {
