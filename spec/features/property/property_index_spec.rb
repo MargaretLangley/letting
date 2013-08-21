60b8117de4f99e0b05da7cfdb04ee7483b9741da
Richard Wigley <Richard.Wigley@gmail.com>
2013-08-21 08:21:38 +0100

    CHANGE added feature test for property search

--- spec/features/property/property_index_spec.rb
+++ spec/features/property/property_index_spec.rb
@@ -18,4 +18,19 @@ describe Property do
     expect_index_address_edgbaston
   end
 
+  it '#index search' do
+    property_factory human_id: 111,
+              address_attributes: { road: 'Vauxall Lane' }
+    property_factory human_id: 222
+    property_factory human_id: 333
+
+    visit '/properties'
+
+    fill_in 'search', with: 'Edgbaston Road'
+    click_on 'Search'
+    expect(page).to_not have_text '111'
+    expect(page).to have_text '222'
+    expect(page).to have_text '333'
+  end
+
 end
