4e7dd10f445b34bdc1900176104984c81d1f79bc
Richard Wigley <Richard.Wigley@gmail.com>
2013-08-24 22:41:20 +0100

    FEATURE Disable button when we have run out of panels to reveal

--- app/assets/javascripts/reveal.js
+++ app/assets/javascripts/reveal.js
@@ -2,7 +2,14 @@ $( document ).ready(function() {
 
   $('.add').click(function(event) {
     event.preventDefault();
-    $(this).closest('.selection').find(':hidden:first').slideToggle('fast');
+    var selection = $(this).closest('.selection');
+    selection.find(':hidden:first').slideToggle('fast');
+    if(selection.find(':hidden').length > 0) {
+      // todo
+    } else {
+      $(this).addClass('disabled');
+      // note disabled class is in forms.css.scss unless brighter idea
+    }
   });
 
 });
