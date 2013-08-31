a4d63aa245d5f970d4e319ff624b739428a81e43
Margaret <margaret.b.langley@gmail.com>
2013-08-31 12:11:36 +0100

    UPDATE javascript code tidy for debug to work

--- app/assets/javascripts/change_element_display.js
+++ app/assets/javascripts/change_element_display.js
@@ -10,18 +10,6 @@ $(function () {
     $("#agent_address").css("display", "block");
 }
 
-
-  $("#company_wanted").click(function(event){
-    //  $("#company_name").css("display", "block");
-    //  $("#client_entity_0").css("display", "none");
-    // $("#client_entity_0 > h3").html("Person/Company");
-    // $("#client_entities_attributes_0_title").siblings("label").css("display", "none");
-    // $("#client_entities_attributes_0_initials").siblings("label").css("display", "none");
-    // $("#client_entities_attributes_0_initials").css("display", "none");
-    // $("#client_entities_attributes_0_title").css("display", "none");
-    //  event.preventDefault();
-  });
-
 $("#property_entity_add").click(function(event){
   $("#property_entity_1").css("display", "block");
   event.preventDefault();
