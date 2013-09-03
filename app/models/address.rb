34bf31e197604802b8f90c5b1f2f563ce9246dd0
Richard Wigley <Richard.Wigley@gmail.com>
2013-09-03 17:04:09 +0100

    CHANGE Removed requirement for having a road number or house name in the address.

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,9 +2,7 @@ class Address < ActiveRecord::Base
   belongs_to :addressable, polymorphic: true
   validates :county, :road, presence: true
   validates :flat_no, length: { maximum: 10 }, allow_blank: true
-  validates :house_name, presence: true, unless: :road_no?
   validates :house_name, length: { maximum: 64 }, allow_blank: true
-  validates :road_no, presence: true, unless: :house_name?
   validates :road_no, length: { maximum: 10 }, allow_blank: true
   validates :road, length: { maximum: 64 }
   validates :district, :town, length: { minimum: 4, maximum: 64 }, allow_blank: true
