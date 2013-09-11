1459862ae3bc6fe40d9b5d246d1ad9140e0f5594
Richard Wigley <Richard.Wigley@gmail.com>
2013-09-11 15:11:15 +0100

    FEATURE rspec focus on running certain tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,8 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
