f6e620a3cfd816d67b4f6e70d2ec35cb92751d37
Richard Wigley <Richard.Wigley@gmail.com>
2013-09-11 18:54:11 +0100

    CHANGE

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,11 +1,10 @@
 class Permission < Struct.new(:user)
   def allow?(controller, action)
-    # return true if controller == 'sessions'
-    # if user
-    #   return true if controller == 'blocks'
-    #   return true if controller == 'clients'
-    #   return true if controller == 'properties'
-    # end
-    true
+    return true if controller == 'sessions'
+    if user
+      return true if controller == 'blocks'
+      return true if controller == 'clients'
+      return true if controller == 'properties'
+    end
   end
 end
