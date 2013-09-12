042b275d95a66b98d4c0d37af368caf3f95e75a4
Richard Wigley <Richard.Wigley@gmail.com>
2013-09-12 12:56:39 +0100

    CHANGE prawn asked for in production ... removed until someone can look at it

--- app/pdfs/letter_pdf.rb
+++ app/pdfs/letter_pdf.rb
@@ -1,25 +1,30 @@
-class LetterPdf < Prawn::Document
-  def initialize(property)
-    @property = property
-    super(top_margin: 70)
-    text "#{@property.human_id}"
-    text "#{address_road()}"
-    text "#{@property.address.district}"
-    text "#{@property.address.town}"
-    text "#{@property.address.county}"
-    text "#{@property.address.postcode}"
-   end
+# Commented out because in production it is asking for prawn
+# Does it need to be added to Gemfile?
 
-private
-def address_road()
-  if  @property.address.road_no.length > 0
-  address_road = @property.address.road_no + " " + @property.address.road
-else
-  address_road =  @property.address.road
- end
 
-end
 
-end
+# class LetterPdf < Prawn::Document
+#   def initialize(property)
+#     @property = property
+#     super(top_margin: 70)
+#     text "#{@property.human_id}"
+#     text "#{address_road()}"
+#     text "#{@property.address.district}"
+#     text "#{@property.address.town}"
+#     text "#{@property.address.county}"
+#     text "#{@property.address.postcode}"
+#    end
+
+# private
+# def address_road()
+#   if  @property.address.road_no.length > 0
+#   address_road = @property.address.road_no + " " + @property.address.road
+# else
+#   address_road =  @property.address.road
+#  end
+
+# end
+
+# end
 
 
