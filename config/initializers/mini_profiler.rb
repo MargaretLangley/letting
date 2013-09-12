afa3fcc347edcae9d4461eb86e33f2a5f621f1a1
Richard Wigley <Richard.Wigley@gmail.com>
2013-09-12 12:51:05 +0100

    FEATURE MiniProfiler Initializer only used in dev

--- config/initializers/mini_profiler.rb
+++ config/initializers/mini_profiler.rb
@@ -1,2 +1,4 @@
-Rack::MiniProfiler.config.position = 'right'
-Rack::MiniProfiler.config.start_hidden = true
+if Rails.env.development?
+  Rack::MiniProfiler.config.position = 'right'
+  Rack::MiniProfiler.config.start_hidden = true
+end
