d07d87c692af4bba8ee5c654898984b849bf91e3
Margaret <margaret.b.langley@gmail.com>
2013-09-14 12:08:17 +0100

    REFACTOR

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :email, presence: true, format: { with: /@/ }, uniqueness: true
   validates :password_digest, presence: true
-  validates_presence_of :password, :on => :create
+  validates :password, presence: true, on: :create
 end
