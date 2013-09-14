ef87a00bfcd603124c43dcb304ff27a80b84e463
Richard Wigley <Richard.Wigley@gmail.com>
2013-09-14 16:17:04 +0100

    REFACTOR Addresss Search

--- app/models/address.rb
+++ app/models/address.rb
@@ -10,10 +10,10 @@ class Address < ActiveRecord::Base
   validates :postcode, length: { minimum: 6, maximum: 8 }, allow_blank: true
 
   scope :search_by_all, ->(search) {
-    where( 'addresses.house_name ILIKE ? OR ' + \
-              'addresses.road ILIKE ? OR '  \
-              'addresses.town ILIKE ?',  \
-              "#{search}%", "#{search}%", "#{search}%" \
+    where( 'addresses.house_name ILIKE :s  OR ' + \
+              'addresses.road ILIKE :s  OR '  \
+              'addresses.town ILIKE :s ',  \
+              s: "#{search}%" \
     )}
 
   def empty?
