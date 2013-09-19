6eedbc69e68a04cc31ad8ee807a952b53b412cd1
Margaret <margaret.b.langley@gmail.com>
2013-09-19 06:38:08 +0100

    FEATURE route to payments page

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -5,6 +5,7 @@ class Permission < Struct.new(:user)
       return true if controller == 'blocks'
       return true if controller == 'clients'
       return true if controller == 'properties'
+      return true if controller == 'payments'
       if user.admin?
         return true if controller == 'users'
       end
