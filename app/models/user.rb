038eb09107d08d7f59e30ecbe4d973554e5a785c
Richard Wigley <Richard.Wigley@gmail.com>
2013-09-19 16:56:52 +0100

    FIX Stop multiple lines of email addresses being acceptable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :email, presence: true, format: { with: /@/ }, uniqueness: true
+  validates :email, presence: true, format: { with: /\A.*@.*\z/ }, uniqueness: true
   validates :password_digest, presence: true
   validates :password, presence: true, on: :create
 end
