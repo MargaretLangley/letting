46477b5f1e38a43baed80b1883583160c2029708
Richard Wigley <Richard.Wigley@gmail.com>
2013-09-20 11:02:18 +0100

    FEATURE Index to new dispalays properties in bloxk

--- app/controllers/debts_controller.rb
+++ app/controllers/debts_controller.rb
@@ -5,6 +5,10 @@ class DebtsController < ApplicationController
   end
 
   def new
+    properties = nil
+    if params[:block].present?
+      @properties = Property.search_by_house_name params[:block]
+    end
   end
 
   def create
