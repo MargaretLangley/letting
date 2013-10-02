b570eddf35bd597d2a623ae91debf152ac5e9dea
Richard Wigley <Richard.Wigley@gmail.com>
2013-10-02 17:00:38 +0100

    REFACTOR

--- spec/models/debt_generator_spec.rb
+++ spec/models/debt_generator_spec.rb
@@ -2,43 +2,20 @@ require 'spec_helper'
 
 describe DebtGenerator do
 
-  let(:debt_gen) { DebtGenerator.new }
+  let(:debt_gen) { debt_generator_new }
 
   context 'validations' do
+    it 'basic is valid' do
+      expect(debt_gen).to be_valid
+    end
+    it 'invalid with no debts' do
+      debt_gen.debts = []
+      expect(debt_gen).to_not be_valid
+    end
     context 'validate uniqueness' do
       it 'prevents same debt_generator being created' do
-        DebtGenerator.create! debt_generator_attributes id: nil
-        expect{ DebtGenerator.create! debt_generator_attributes id: nil}.to \
-          raise_error ActiveRecord::RecordInvalid
-      end
-    end
-  end
-
-  context 'inialization' do
-    context 'empty' do
-      before { Timecop.travel(Time.zone.parse('30/9/2013 12:00')) }
-      after { Timecop.return }
-
-      let(:debt_gen) { debt_gen = DebtGenerator.new }
-
-      it 'returns today start date' do
-        expect(debt_gen.start_date).to eq Date.new 2013, 9, 30
-      end
-
-      it 'returns future date' do
-        expect(debt_gen.end_date).to eq Date.new 2013, 11, 25
-      end
-    end
-    context 'with parsable strings' do
-      let(:debt_gen) do
-        debt_gen = DebtGenerator.new \
-                    search_string: 'Lords', \
-                    start_date: '2013-09-30', \
-                    end_date: '2013-11-25'
-      end
-
-      it 'returns date' do
-        expect(debt_gen.start_date).to eq Date.new 2013, 9, 30
+        debt_generator_new.save!
+        expect{ debt_generator_new.save! }.to raise_error ActiveRecord::RecordInvalid
       end
     end
   end
@@ -47,13 +24,47 @@ describe DebtGenerator do
     it('debts') { expect(debt_gen).to respond_to(:debts)}
   end
 
+  context 'inialization' do
+    context 'new' do
+      let(:debt_gen) { debt_gen = DebtGenerator.new }
+      before { Timecop.travel(Time.zone.parse('30/9/2013 12:00')) }
+      after { Timecop.return }
+
+      it('has start date') { expect(debt_gen.start_date).to eq Date.new 2013, 9, 30 }
+      it('has end date') { expect(debt_gen.end_date).to eq Date.new 2013, 11, 25 }
+    end
+  end
+
   context 'methods' do
-    context '#debtless?' do
-      it 'when empty' do
-        debt_gen = DebtGenerator.new
-        expect(debt_gen).to be_debtless
+    context '#generate' do
+      property = nil
+      before do
+        Timecop.travel(Time.zone.parse('30/2/2013 12:00'))
+        property = property_with_charge_create!
       end
-      it 'has debts when assigned' do
+      after { Timecop.return }
+
+      it 'generates debts' do
+        gen_debts = DebtGenerator.new(search_string: 'Hillbank House').generate
+        expect(gen_debts).to have(1).items
+        expect(gen_debts.first).to eq \
+          Debt.new on_date: '2013/3/25', amount: 88.08, \
+            charge_id: property.account.charges.first.id
+      end
+
+      it 'does not duplicate debt' do
+        debt_gen = DebtGenerator.new(search_string: 'Hillbank House')
+        debt_gen.generate
+        debt_gen.save!
+        debt_gen = DebtGenerator.new(search_string: 'Hillbank House', start_date: Date.current+1.day)
+        debt_gen.generate
+        expect{ debt_gen.save! }.to raise_error
+      end
+    end
+
+    context '#debtless?' do
+      it('when empty') { expect(DebtGenerator.new).to be_debtless }
+      it 'indebted when debts assigned' do
         debt_gen = DebtGenerator.new
         # setting up debt generation will result in complicated test
         # doing something like debt_gen.debts << Debt.new would use
@@ -62,47 +73,23 @@ describe DebtGenerator do
         expect(debt_gen).to_not be_debtless
       end
     end
-    context '#generate' do
-      property = nil
-      before do
-       Timecop.travel(Time.zone.parse('30/2/2013 12:00'))
-       property = property_with_charge_create!
-      end
-      after { Timecop.return }
 
-      it 'generates' do
-        gen_debts = DebtGenerator.new(search_string: 'Hillbank House').generate
-        expect(gen_debts).to have(1).items
-        expect(gen_debts.first).to eq \
-          Debt.new on_date: '2013/3/25', amount: 88.08, \
-            charge_id: property.account.charges.first.id
-      end
-      it 'does not duplicate debt' do
-        debt_gen = DebtGenerator.new(search_string: 'Hillbank House')
-        debt_gen.generate
-        debt_gen.generate
-        expect(debt_gen.debts).to have(1).items
-      end
-      it 'does not duplicate debt2' do
-        debt_gen = DebtGenerator.new(search_string: 'Hillbank House')
-        debt_gen.generate
-        debt_gen.save!
-        debt_gen = DebtGenerator.new(search_string: 'Hillbank House', start_date: Date.current+1.day)
-        debt_gen.generate
-        debt_gen.save!
-        expect(debt_gen.debts).to have(0).items
-      end
+    it '#latest_debt_generated' do
+      debt_generator_new.save!
+      expect(DebtGenerator.latest_debt_generated(10).length).to eq 1
+      expect(DebtGenerator.latest_debt_generated(10).first).to eq \
+          DebtGenerator.new id: 1, \
+                            search_string: 'Lords', \
+                            start_date: '2013-03-01', \
+                            end_date: '2013-04-01'
     end
   end
 
-  it '#latest_debt_generated' do
-    DebtGenerator.create debt_generator_attributes
-    expect(DebtGenerator.latest_debt_generated(10).length).to eq 1
-    expect(DebtGenerator.latest_debt_generated(10).first).to eq \
-        DebtGenerator.new id: 1, \
-                          search_string: 'Lords', \
-                          start_date: '2013-03-01', \
-                          end_date: '2013-04-01'
+
+  def debt_generator_new
+    debt_gen = DebtGenerator.new debt_generator_attributes
+    debt_gen.debts.build debt_attributes
+    debt_gen
   end
 
 end
