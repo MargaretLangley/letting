be04fa2dc9a1020cabf2b06584750f38b9675f99
Richard Wigley <Richard.Wigley@gmail.com>
2013-10-03 18:05:48 +0100

    REFACTOR Rubocop

--- spec/lib/import/import_fields_spec.rb
+++ spec/lib/import/import_fields_spec.rb
@@ -6,20 +6,35 @@ module DB
   describe ImportFields do
 
    it 'client' do
-      fields = %w[human_id title1 initials1 name1 title2
-        initials2 name2 flat_no  house_name road_no  road  district  town  county  postcode]
+      fields =
+      %w[
+        human_id
+        title1  initials1 name1
+        title2 initials2 name2
+        flat_no house_name road_no road district town county postcode
+      ]
       expect(ImportFields.client).to eq fields
    end
 
    it 'property' do
-      fields = %w[human_id  updated title1  initials1 name1 title2
-        initials2 name2 flat_no  house_name road_no  road  district  town  county  postcode client_id]
+      fields =
+      %w[
+        human_id  updated
+        title1 initials1 name1
+        title2 initials2 name2
+        flat_no house_name road_no road district town county postcode client_id
+      ]
       expect(ImportFields.property).to eq fields
    end
 
    it 'billing_profile' do
-      fields = %w[human_id  title1  initials1 name1 title2
-        initials2 name2 flat_no  house_name road_no  road  district  town  county  postcode]
+      fields =
+      %w[
+        human_id
+        title1 initials1 name1
+        title2 initials2 name2
+        flat_no house_name road_no road district town county postcode
+      ]
       expect(ImportFields.billing_profile).to eq fields
    end
 
