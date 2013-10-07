a22623362366ef25bb44648bbd1aead790a0e1b5
Richard Wigley <Richard.Wigley@gmail.com>
2013-10-07 14:33:00 +0100

    REFACTOR routes - weren't being used

--- config/routes.rb
+++ config/routes.rb
@@ -2,29 +2,21 @@ Letting::Application.routes.draw do
 
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
-
+  resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
-  resources :search_suggestions
-
   root 'properties#index'
-
-  resources :blocks, only: [:new, :create, :index]
-
   resources :properties do
     collection do
       get :search
     end
   end
+  resources :blocks, only: [:new, :create, :index]
+  resources :search_suggestions
   resources :clients
-  resources :charges
-  resources :debts do
-    collection do
-      put :apply
-    end
-  end
-  resources :payments
-  resources :users
 
+  resources :charges
   resources :debt_generators
+  resources :payments
+
 end
