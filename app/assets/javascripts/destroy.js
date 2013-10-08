9419ca9b4c0a7b587d75204dc26593aaa9be1ac6
Richard Wigley <Richard.Wigley@gmail.com>
2013-10-08 10:31:23 +0100

    REFACTOR as last message

--- app/assets/javascripts/destroy.js
+++
@@ -1,8 +0,0 @@
-
-$( document ).ready(function() {
-  $('.remove-fields').click(function(event) {
-    event.preventDefault();
-    $(this).prev('input[type=hidden]').val('1');
-    $(this).closest('.selection').slideToggle('fast');
-  });
-});
