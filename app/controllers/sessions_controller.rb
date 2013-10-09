ccfff925a90f8cdb3907029c77215974c897857c
Richard Wigley <Richard.Wigley@gmail.com>
2013-10-09 22:41:43 +0100

    DOCUMENTATION + REFACTOR session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,28 @@
+#####
+#
+# SessionsController
+#
+# Handles the authentication into the sysem.
+#
+# Users 'log into' the system when a create action occurs.
+# Users 'log out' of the system when a destroy action occurs.
+#
+# When a user authenticates the user_id it is saved in the session.
+# The application acesses the user_id through the application_controller using
+# the current_user. Each request has the session's user_id verify against the
+# the application database's users table.
+#
+#####
+#
 class SessionsController < ApplicationController
-  def new
-  end
 
   def create
-    user = User.find_by email: params[:email]
+    user = user_from_email
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_path, notice: 'Logged in!'
     else
-      flash.now.alert = 'Email or password is invalid'
+      alert_authentication_failed
       render 'new'
     end
   end
@@ -16,5 +30,15 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to login_path, notice: 'Logged out!'
+  end
+
+  private
+
+  def user_from_email
+    User.find_by email: params[:email]
+  end
+
+  def alert_authentication_failed
+    flash.now.alert = 'Email or password is invalid'
   end
 end
