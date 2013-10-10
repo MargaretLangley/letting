28732a9a735115e9b8077de30f89dfe9de061ad9
Richard Wigley <Richard.Wigley@gmail.com>
2013-10-10 10:03:06 +0100

    DOCUMENTATION

--- app/models/chargeable_info.rb
+++ app/models/chargeable_info.rb
@@ -1,17 +1,28 @@
+####
+#
+# ChargeableInfo
+#
+#  Holds information to turn a charge into a debt
+#
+#  The debt generator needs to generate debts from charges.
+#  It calls on account for due charges and in turn charge
+#  generates a ChargeableInfo describing a charge that is due
+#  in the queried date range.
+#
 class ChargeableInfo
   attr_reader :account_id, :charge_id, :on_date, :amount
 
   def self.from_charge args = {}
-    new charge_id: args[:charge_id],
-        on_date:   args[:on_date],
-        amount:    args[:amount],
+    new charge_id:  args[:charge_id],
+        on_date:    args[:on_date],
+        amount:     args[:amount],
         account_id: args[:account_id]
   end
 
   def == other
-    charge_id == other.charge_id &&
-    on_date == other.on_date &&
-    amount == other.amount &&
+    charge_id  == other.charge_id &&
+    on_date    == other.on_date &&
+    amount     == other.amount &&
     account_id == other.account_id
   end
 
@@ -25,9 +36,9 @@ class ChargeableInfo
   private
 
   def initialize args = {}
-    @charge_id = args[:charge_id]
-    @on_date = args[:on_date]
-    @amount = args[:amount]
+    @charge_id  = args[:charge_id]
+    @on_date    = args[:on_date]
+    @amount     = args[:amount]
     @account_id = args[:account_id]
   end
 end
