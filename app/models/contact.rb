6ee2b83235f9b3dc9b9f39cbd818c0f46c840ba5
Richard Wigley <Richard.Wigley@gmail.com>
2013-10-10 14:01:01 +0100

    DOCUMENTATION

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,21 @@
+####
+#
+# module Contact
+#
+# The system follows entities with an address this wraps up the relationship
+#
+# Contact is used by Property, Client and Billing Address.
+# Contact includes entites and an address.
+####
+#
 module Contact
   extend ActiveSupport::Concern
   include Entities
   included do
     accepts_nested_attributes_for :entities, allow_destroy: true,
-                                             reject_if: :all_blank
-    has_one :address, class_name: 'Address', dependent: :destroy,
+                                             reject_if:     :all_blank
+    has_one :address, class_name: 'Address',
+                      dependent: :destroy,
                       as: :addressable
     accepts_nested_attributes_for :address, allow_destroy: true
   end
