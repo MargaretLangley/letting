c04cc08f6ba7bf1433e916b0a1131857c1c87b03
Richard Wigley <Richard.Wigley@gmail.com>
2013-10-11 21:44:59 +0100

    REFACTOR

--- spec/support/auth_macros.rb
+++ spec/support/auth_macros.rb
@@ -3,11 +3,15 @@ module AuthMacros
   def log_in attributes = {}
     current_user = User.create! user_attributes attributes
     visit '/login/'
+    login_form current_user
+    expect(page).to have_content 'Logged in'
+  end
+
+  def login_form current_user
     within_fieldset 'login' do
       fill_in 'Email',    with: current_user.email
       fill_in 'Password', with: current_user.password
       click_on 'Log In'
     end
-    expect(page).to have_content 'Logged in'
   end
 end
