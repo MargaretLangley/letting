d9271633f913735e4bb59faaeeba777e0fec14b3
Richard Wigley <Richard.Wigley@gmail.com>
2013-10-11 15:04:08 +0100

    DOCUMENTATION debt_generator_helper - removed unused code

--- app/helpers/debt_generators_helper.rb
+++ app/helpers/debt_generators_helper.rb
@@ -1,14 +1,10 @@
+####
+#
+# module DebtGeneratorsHelper
+#
+# Provides a wrapper for the debt_generator class
+#
 module DebtGeneratorsHelper
-  # debt_generator isn't saved on search query only on a create
-  # consequently. the params[:seach_start_date] can be changed while
-  # debt_generator.start_date will remain the same (the default)
-  def start_date debt_generator
-    params[:search_start_date] || debt_generator.start_date
-  end
-
-  def end_date debt_generator
-    params[:search_end_date] || debt_generator.end_date
-  end
 
   def search_range
     "#{@debt_generator.start_date.to_formatted_s(:day_and_month)} - " +
