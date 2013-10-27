024f9bce716da5dde7d7ea3af205774daf9054f5
Richard Wigley <Richard.Wigley@gmail.com>
2013-10-27 22:17:13 +0000

    REFACTOR asking charge to clean up form rather than telling it

--- app/models/charges.rb
+++ app/models/charges.rb
@@ -38,18 +38,6 @@ module Charges
 
       def clean_up_form
         each { |charge| charge.clean_up_form }
-        destruction_if :empty?
-      end
-
-  private
-
-      def destruction_if matcher
-        select(&matcher)
-        .each { |charge| mark_charge_for_destruction charge }
-      end
-
-      def mark_charge_for_destruction charge
-        charge.mark_for_destruction
       end
     end
   end
