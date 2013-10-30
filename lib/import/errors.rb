9ac86d291ce29d58cd619c314ce81d1ed791ef56
Richard Wigley <Richard.Wigley@gmail.com>
2013-10-30 14:56:35 +0000

    FEATURE import acccount errors

--- lib/import/errors.rb
+++ lib/import/errors.rb
@@ -3,4 +3,6 @@ module DB
   class CreditNegativeError < Error; end
   class BalanceNotMatching < Error; end
   class ChargeUnknown < Error; end
+  class ChargeCodeUnknown < Error; end
+  class PropertyRefUnknown < Error; end
 end
