eb1c05bd3d893812f78d9da54f01816ced50cf0a
Richard Wigley <Richard.Wigley@gmail.com>
2013-10-30 14:55:47 +0000

    FEATURE credit row added for account import feature

---
+++ lib/import/credit_row.rb
@@ -0,0 +1,79 @@
+require_relative 'charge_code'
+require_relative 'errors'
+
+module DB
+
+  class CreditRow
+    def initialize row
+      @row = row
+    end
+
+    def human_ref
+      @row[:human_ref]
+    end
+
+    def charge_code
+      @row[:charge_code]
+    end
+
+    def date
+      @row[:on_date]
+    end
+
+    def amount
+      @row[:credit].to_f
+    end
+
+    def account_id
+      Property.find_by!(human_ref: human_ref).account.id
+      rescue ActiveRecord::RecordNotFound
+        raise DB::PropertyRefUnknown, property_unknown_message, caller
+    end
+
+    def charge_type
+      charge = ChargeCode.to_string charge_code
+      raise DB::ChargeCodeUnknown, charge_code_message, caller unless charge
+      charge
+    end
+
+    def attributes
+      credit_attributes
+    end
+
+    def method_missing method_name, *args, &block
+      @row.send method_name, *args, &block
+    end
+
+    def respond_to_missing? method_name, include_private = false
+      @row.respond_to?(method_name, include_private) || super
+    end
+
+    private
+
+    def payment_attributes
+      {
+        account_id: -1,  # FIND!
+        on_date: date,
+        amount: amount,
+      }
+    end
+
+
+    def credit_attributes
+      {
+        payment_id: -1,
+        on_date: date,
+        amount: amount,
+      }
+    end
+
+    def charge_code_message
+      "Charge code #{charge_code} can not be converted into a string"
+    end
+
+    def property_unknown_message
+      "Property ref: #{human_ref} is unknown."
+    end
+
+  end
+end
---
+++ spec/lib/import/credit_row_spec.rb
@@ -0,0 +1,76 @@
+require 'csv'
+require 'spec_helper'
+require_relative '../../../lib/import/file_header'
+require_relative '../../../lib/import/credit_row'
+
+module DB
+  describe CreditRow do
+
+    context 'credit row' do
+      let(:row) { CreditRow.new parse_line credit_row }
+
+      context 'readers' do
+
+        it 'human_ref' do
+          expect(row.human_ref).to eq '89'
+        end
+
+        it 'charge_code' do
+          expect(row.charge_code).to eq 'GR'
+        end
+
+        it 'date' do
+          expect(row.date).to eq '2012-03-25 12:00:00'
+        end
+
+        it 'amount' do
+          expect(row.amount).to eq 50.5
+        end
+
+      end
+
+      context 'methods' do
+
+        context '#ChargeType' do
+
+          it 'returns valid' do
+            expect(row.charge_type).to eq 'Ground Rent'
+          end
+
+          it 'errors invalid' do
+            bad_code = CreditRow.new parse_line credit_row_no_type
+            expect { bad_code.charge_type }.to raise_error ChargeCodeUnknown
+          end
+
+        end
+
+        context '#account_id' do
+          it 'returns valid' do
+            property_create! human_ref: 89
+            expect(row.account_id).to eq 1
+          end
+          it 'errors invalid' do
+            expect{ row.account_id }.to raise_error PropertyRefUnknown
+          end
+        end
+      end
+    end
+
+    def parse_line row_string
+      CSV.parse_line( row_string,
+                      { headers: FileHeader.account,
+                        header_converters: :symbol,
+                        converters: lambda { |f| f ? f.strip : nil } }
+                    )
+    end
+
+    def credit_row
+      %q[89, GR, 2012-03-25 12:00:00, Ground Rent, 0, 50.5, 0]
+    end
+
+    def credit_row_no_type
+      %q[89, XX, 2012-03-25 12:00:00, Ground Rent, 0, 50.5, 0]
+    end
+
+  end
+end
