97a2ef02a6915c92354892a52b27c72d5f32aa1a
Richard Wigley <Richard.Wigley@gmail.com>
2013-11-02 10:38:56 +0000

    CHANGE import_property from files to strings. Added test for filtering range.

--- spec/lib/import/import_billing_profile_spec.rb
+++ spec/lib/import/import_billing_profile_spec.rb
@@ -11,78 +11,87 @@ module DB
       property_create! human_ref: 122
     end
 
+    def row
+      %q[122, Mr, B P, Example, Mrs, A N, Other,] +
+      %q[1, ExampleHouse, 2, Ex Street, ,Ex Town, Ex County, E10 7EX, ]
+    end
+
     it 'One row' do
       expect(BillingProfile.first.use_profile).to be_false
-      ImportBillingProfile.import billing_csv
+      expect { import_billing row }.to_not change(BillingProfile, :count)
       expect(BillingProfile.first.use_profile).to be_true
     end
 
-    it 'One row, 2 Entities' do
-      expect { ImportBillingProfile.import billing_csv }.to \
-        change(Entity, :count).by 2
-    end
+    context 'filter' do
+      it 'allows within range' do
+        import_billing row, range: 122..122
+        expect(BillingProfile.first.use_profile).to be_true
+      end
 
-    it 'billing profiles have already been created when property created' do
-      expect { ImportBillingProfile.import billing_csv }.to_not \
-        change(BillingProfile, :count)
-    end
-
-    it 'Not double import' do
-      expect { ImportBillingProfile.import billing_csv }.to \
-        change(Entity, :count).by 2
-      expect { ImportBillingProfile.import billing_csv }.to_not \
-        change(Entity, :count)
+      it 'filters if out of range' do
+        import_billing row, range: 120..121
+        expect(BillingProfile.first.use_profile).to be_false
+      end
     end
 
     context 'entities' do
+
+      def row_1_entity
+        %q[122, Mr, B P, Example, , , ,] +
+        %q[1, ExampleHouse, 2, Ex Street, ,Ex Town, Ex County, E10 7EX, ]
+      end
+
+      it 'One row, 2 Entities' do
+        expect { import_billing row }.to change(Entity, :count).by 2
+      end
+
       it 'adds one entity when second entity blank' do
-        expect { ImportBillingProfile.import billing_with_1_entity_csv }.to \
-          change(Entity, :count).by 1
+        expect { import_billing row_1_entity }.to change(Entity, :count).by 1
       end
 
       it 'ordered by creation' do
-        ImportBillingProfile.import billing_csv
-        expect(BillingProfile.first.entities[0].created_at).to be \
-          < BillingProfile.first.entities[1].created_at
+        import_billing row
+        entities = BillingProfile.first.entities
+        expect(entities[0].created_at).to be < entities[1].created_at
       end
 
-      context 'multiple imports' do
+      context 'double imports on entities' do
 
-        it 'updated changed entities' do
-          ImportBillingProfile.import billing_csv
-          ImportBillingProfile.import billing_updated_csv
+        it 'count stays the same' do
+          expect { import_billing row }.to change(Entity, :count).by 2
+          expect { import_billing row }.to_not change(Entity, :count)
+        end
+
+        def changed_row
+          %q[122, Mr, B P, Changed, Mrs, A N, Other,] +
+          %q[1, ExampleHouse, 2, Ex Street, ,Ex Town, Ex County, E10 7EX, ]
+        end
+
+        it 'updates values' do
+          import_billing row
+          import_billing changed_row
           expect(BillingProfile.first.entities[0].name).to eq 'Changed'
           expect(BillingProfile.first.entities[1].name).to eq 'Other'
         end
 
         it 'removes deleted second entities' do
-          ImportBillingProfile.import billing_csv
-          expect { ImportBillingProfile.import billing_with_1_entity_csv }.to \
+          import_billing row
+          expect { import_billing row_1_entity }.to \
             change(Entity, :count).by(-1)
         end
       end
     end
 
-    def billing_csv
-      FileImport.to_a('address2',
-                      headers: FileHeader.billing_profile,
-                      location: billing_profile_dir)
+    def import_billing row, args = {}
+      ImportBillingProfile.import parse(row), args
     end
 
-    def billing_updated_csv
-      FileImport.to_a 'address2_updated',
-                      headers: FileHeader.billing_profile,
-                      location: billing_profile_dir
-    end
-
-    def billing_with_1_entity_csv
-      FileImport.to_a 'address2_one_entity',
-                      headers: FileHeader.billing_profile,
-                      location: billing_profile_dir
-    end
-
-    def billing_profile_dir
-      'spec/fixtures/import_data/billing_profiles'
+    def parse row_string
+      CSV.parse( row_string,
+                 { headers: FileHeader.billing_profile,
+                   header_converters: :symbol,
+                   converters: lambda { |f| f ? f.strip : nil } }
+               )
     end
   end
 end
