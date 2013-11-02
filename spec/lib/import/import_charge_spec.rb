79fac692914e4058378ccb2487a310c63c30bd2b
Richard Wigley <Richard.Wigley@gmail.com>
2013-11-02 11:17:25 +0000

    CHANGE charges use strings instead of files. Test for filtering of charges

--- spec/lib/import/import_charge_spec.rb
+++ spec/lib/import/import_charge_spec.rb
@@ -8,63 +8,84 @@ module DB
 
   describe ImportCharge, :import do
 
-    it 'One row' do
-      property_create!
-      expect { ImportCharge.import charge_csv }.to change(Charge, :count).by 1
+    def row
+      %q[2002, 2006-12-30 17:17:00, GR, 0, 40.5,  S,] +
+      %q[24, 6, 25, 12,  0,  0,  0,  0, 1900-01-01 00:00:00, 0 ]
     end
 
-    it 'fails if property does not exist' do
-      expect { ImportCharge.import charge_csv }.to \
-      raise_error ActiveRecord::RecordNotFound,
-                  'Property human_ref: 2002 - Not found'
-    end
-
-    it 'One row, 2 DueOns' do
-      property_create!
-      expect { ImportCharge.import charge_csv }.to change(DueOn, :count).by 2
-    end
-
-    it 'One monthly row, 12 DueOns' do
-      property_create!
-      expect { ImportCharge.import charge_monthly_csv }.to \
-        change(DueOn, :count).by 12
-    end
-
-    it 'Not double import' do
-      property_create!
-      ImportCharge.import charge_csv
-      expect { ImportCharge.import charge_csv }.to_not change(Charge, :count)
-    end
-
-    context 'multiple imports' do
-      it 'updated changed charge' do
+    context 'success' do
+      before :each do
         property_create!
-        ImportCharge.import charge_csv
-        ImportCharge.import charge_updated_csv
-        charge = Charge.first
-        expect(charge.amount).to eq 30.5 #
-        expect(charge.due_ons[0].day).to eq 24 # same
-        expect(charge.due_ons[1].day).to eq 26 # imp one!
+      end
+
+      it 'One row' do
+        expect { import_charge row }.to change(Charge, :count).by 1
+      end
+
+      it 'One row, 2 DueOns' do
+        expect { import_charge row }.to change(DueOn, :count).by 2
+      end
+
+      it 'One monthly row, 12 DueOns' do
+        def monthly_row
+          %q[2002, 2006-12-30 17:17:00, GR, 0, 40.5,  S,] +
+          %q[1, 0, 0, 0,  0,  0,  0,  0, 1900-01-01 00:00:00, 0 ]
+        end
+        expect { import_charge monthly_row }.to change(DueOn, :count).by 12
+      end
+
+      context 'filter' do
+        it 'allows within range' do
+          expect { import_charge row, range: 2002..2002 }.to \
+            change(Charge, :count).by 1
+        end
+
+        it 'filters if out of range' do
+          expect { import_charge row, range: 2000..2001 }.to \
+            change(Charge, :count).by 0
+        end
+      end
+
+      it 'Not double import' do
+        import_charge row
+        expect { import_charge row }.to_not change(Charge, :count)
+      end
+
+      context 'multiple imports' do
+
+        def updated_row
+          %q[2002, 2006-12-30 17:17:00, GR, 0, 30.5,  S,] +
+          %q[24, 6, 26, 12,  0,  0,  0,  0, 1900-01-01 00:00:00, 0 ]
+        end
+
+        it 'updated changed charge' do
+          import_charge row
+          import_charge updated_row
+          charge = Charge.first
+          expect(charge.amount).to eq 30.5 #
+          expect(charge.due_ons[0].day).to eq 24 # same
+          expect(charge.due_ons[1].day).to eq 26 # imp one!
+        end
       end
     end
 
-    def charge_csv
-      FileImport.to_a('acc_info',
-                      headers: FileHeader.charge,
-                      location: 'spec/fixtures/import_data/charges')
+    context 'errors' do
+      it 'fails if property does not exist' do
+        expect { import_charge row }.to raise_error ActiveRecord::RecordNotFound,
+                                      'Property human_ref: 2002 - Not found'
+      end
     end
 
-    def charge_updated_csv
-      FileImport.to_a('acc_info_updated',
-                      headers: FileHeader.charge,
-                      location: 'spec/fixtures/import_data/charges')
+    def import_charge row, args = {}
+      ImportCharge.import parse(row), args
     end
 
-    def charge_monthly_csv
-      FileImport.to_a('acc_info_monthly',
-                      headers: FileHeader.charge,
-                      location: 'spec/fixtures/import_data/charges')
+    def parse row_string
+      CSV.parse( row_string,
+                 { headers: FileHeader.charge,
+                   header_converters: :symbol,
+                   converters: lambda { |f| f ? f.strip : nil } }
+               )
     end
-
   end
 end
