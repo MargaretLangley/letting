4a0d7dff1f35563de95733ee32809601905bbf43
Richard Wigley <Richard.Wigley@gmail.com>
2013-11-08 12:48:27 +0000

    FEATURE update_charge task continued

--- lib/import/update_charge.rb
+++ lib/import/update_charge.rb
@@ -2,24 +2,51 @@ module DB
 
   class UpdateCharge
     def self.do
+      new.update
+    end
+
+    def update
+      add_charge_range_dates
+      update_recent_stopped_charges
+    end
+
+    def add_charge_range_dates
       Charge.all.each do |charge|
-        charge.start_date = earliest_debit_date charge
-        charge.end_date = latest_debit_date(charge) \
-          if latest_debit_date(charge) < within_a_year
+        @charge = charge
+        @charge.start_date = first_debit.on_date if first_debit
+        @charge.end_date = last_debit.on_date if stopped_debiting?
         charge.save!
       end
     end
 
-    def self.earliest_debit_date charge
-      charge.debits(order: :on_date).last.on_date
+    def first_debit
+      @charge.debits(order: :on_date).last
     end
 
-    def self.latest_debit_date charge
-      charge.debits(order: :on_date).first.on_date
+    def last_debit
+      @charge.debits(order: :on_date).first
     end
 
-    def self.within_a_year
+    def stopped_debiting?
+      last_debit && last_debit.on_date < within_a_year
+    end
+
+    def within_a_year
       Date.current - 1.years
+    end
+
+    def update_recent_stopped_charges
+      # It's a hack while only 1 needs to be changed
+      property = Property.find_by(human_ref: 6531)
+      if property
+        charges = property.account.charges
+        charge = charges.find_by(charge_type: 'Insurance')
+        if charge.present?
+          @charge = charge
+          @charge.end_date = last_debit.on_date
+          charge.save!
+        end
+      end
     end
   end
 end
--- spec/lib/import/update_charge_spec.rb
+++ spec/lib/import/update_charge_spec.rb
@@ -7,29 +7,38 @@ module DB
     before { Timecop.travel(Date.new(2013, 11, 01)) }
     after  { Timecop.return }
 
-    it 'finds oldest debit' do
+     it 'leaves alone charges without debits' do
       args = { charge_attributes: { start_date: MIN_DATE ,
                                     end_date: MAX_DATE } }
-      account = account_and_charge_new args
-      account.debits.build debit_attributes id: nil, on_date: '2013-3-25'
-      account.debits.build debit_attributes id: nil, on_date: '2012-3-25'
+      (account = account_and_charge_new args).save!
+      UpdateCharge.do
+      expect(account.charges.first.start_date).to eq Date.new 2000,1,1
+      expect(account.charges.first.end_date).to eq Date.new 2100,1,1
+    end
+
+    it 'updates only the start of recurring charge' do
+      args = { charge_attributes: { start_date: MIN_DATE ,
+                                    end_date: MAX_DATE } }
+      (account = account_and_charge_new args).save!
+      account.debits.build debit_attributes id: nil, on_date: '2013-3-25', charge_id: account.charges.first.id
+      account.debits.build debit_attributes id: nil, on_date: '2012-3-25', charge_id: account.charges.first.id
       account.save!
       UpdateCharge.do
       expect(account.charges.first.start_date).to eq Date.new 2012,3,25
       expect(account.charges.first.end_date).to eq Date.new 2100,1,1
     end
 
-    it 'finds oldest debit' do
+    it 'updates start and end of charge which has finished' do
       args = { charge_attributes: { start_date: MIN_DATE ,
                                     end_date: MAX_DATE } }
-      account = account_and_charge_new args
-      account.debits.build debit_attributes id: nil, on_date: '2012-3-25'
-      account.debits.build debit_attributes id: nil, on_date: '2011-3-25'
+      (account = account_and_charge_new args).save!
+      account.debits.build debit_attributes id: nil, on_date: '2012-3-25', charge_id: account.charges.first.id
+      account.debits.build debit_attributes id: nil, on_date: '2011-3-25', charge_id: account.charges.first.id
       account.save!
       UpdateCharge.do
       expect(account.charges.first.start_date).to eq Date.new 2011,3,25
       expect(account.charges.first.end_date).to eq Date.new 2012,3,25
-
     end
+
   end
 end
