9aba06b888c9f1e622dc8a5f5a1b82c376146985
Margaret <margaret.b.langley@gmail.com>
2013-11-08 18:25:14 +0000

    FEATURE Accounts search and list on properties. Link to Accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -15,7 +15,28 @@
 class AccountsController < ApplicationController
 
   def index
-    @properties = User.all
+    @properties = Property.search(search_param).page(params[:page]).load
   end
+
+  private
+
+    def search_param
+      params[:search]
+    end
+
+    def property_params
+      params.require(:property)
+        .permit :human_ref,
+                :client_id,
+                address_attributes:         address_params,
+                entities_attributes:        entities_params,
+                billing_profile_attributes: billing_profile_params,
+                account_attributes:         account_params
+    end
+
+    def billing_profile_params
+      %i(id property_id use_profile) + [address_attributes: address_params] +
+      [entities_attributes: entities_params]
+    end
 
 end
