06df5cfb4f022277d37432c11ade453114c367a0
Richard Wigley <Richard.Wigley@gmail.com>
2013-11-11 14:26:41 +0000

    FEATURE payment added missing payment specs

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -32,16 +32,25 @@ describe Payment do
     it 'has on_date' do
       expect(payment.on_date).to eq Date.new 2013, 9, 30
     end
-
-    it 'has amount' do
-      expect(payment.on_date).to eq Date.new 2013, 9, 30
-    end
-
   end
 
   context 'methods' do
 
-    it '#prepare_for_form prepares credit' do
+    context '#exists?' do
+      it 'true if account known' do
+        payment = Payment.new
+        payment.account = Account.new id: 100
+        expect(payment).to be_exists
+      end
+
+      it 'false if no account' do
+        payment = Payment.new
+        payment.account = nil
+        expect(payment).to_not be_exists
+      end
+    end
+
+    it '#prepare_for_form creates credit' do
       property_with_unpaid_debit.save!
       payment = Payment.new human_ref: 2002
       payment.account =  Account.by_human_ref 2002
@@ -49,42 +58,43 @@ describe Payment do
       expect(payment.credits).to have(1).items
     end
 
-    it 'prepare_for_form calcualtes default amount' do
-      payment.credits.build amount: 44.08
-      payment.credits.build amount: 44.00
-      payment.prepare_for_form
-      expect(payment.amount).to eq 88.08
+    context '#clear_up_form' do
+      it 'removes empty credits' do
+        Credit.any_instance.stub(:outstanding).and_return(0)
+        payment.credits.build
+        payment.clear_up_form
+        expect(payment.credits.first).to be_marked_for_destruction
+      end
     end
 
+    context '#search' do
+
+      it('returns payments on searched date') do
+        property = property_create!
+        payment = Payment.create! payment_attributes account_id: property
+          .account.id
+        expect(Payment.search(Date.current.to_s)).to eq [payment]
+      end
+
+      it('payments on none-searched dates not returned') do
+        property = property_create!
+        Payment.create! payment_attributes account_id: property.account.id
+        expect((Payment.search('2000-1-1'))).to eq []
+      end
+
+      context 'returns nothing' do
+        it('on empty string') do
+          property = property_create!
+          Payment.create! payment_attributes account_id: property.account.id
+          expect((Payment.search(''))).to eq []
+        end
+
+        it('on corrupt date') do
+          property = property_create!
+          Payment.create! payment_attributes account_id: property.account.id
+          expect((Payment.search('2012-x'))).to eq []
+        end
+      end
+    end
   end
-
-  context '#search' do
-
-    it('no string returns empty') do
-      property = property_create!
-      Payment.create! payment_attributes account_id: property.account.id
-      expect((Payment.search(''))).to eq []
-    end
-
-    it('corrupt date empty ') do
-      property = property_create!
-      Payment.create! payment_attributes account_id: property.account.id
-      expect((Payment.search('2012-x'))).to eq []
-    end
-
-    it('on created date') do
-      property = property_create!
-      payment = Payment.create! payment_attributes account_id: property
-        .account.id
-      expect(Payment.search(Date.current.to_s)).to eq [payment]
-    end
-
-    it('not created date') do
-      property = property_create!
-      Payment.create! payment_attributes account_id: property.account.id
-      expect((Payment.search('2000-1-1'))).to eq []
-    end
-
-  end
-
 end
