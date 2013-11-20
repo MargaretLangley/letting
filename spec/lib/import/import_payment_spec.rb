1d45ac2e0923afb57b32ce0567c59554d12373d9
Richard Wigley <Richard.Wigley@gmail.com>
2013-11-20 12:15:55 +0000

    REFACTOR import_payment

--- spec/lib/import/import_payment_spec.rb
+++ spec/lib/import/import_payment_spec.rb
@@ -14,38 +14,6 @@ module DB
         .to change(Credit, :count).by 1
     end
 
-    it 'without charge_type raises error' do
-      Credit.any_instance.stub(:type).and_return 'Service Charge'
-      (property_with_unpaid_debit human_ref: 89).save!
-
-      expect { ImportPayment.import parse credit_row }
-        .to raise_error DB::ChargeTypeUnknown
-    end
-
-    it 'double import raises error' do
-      Credit.any_instance.stub(:type).and_return 'Ground Rent'
-      (property_with_unpaid_debit human_ref: 89).save!
-
-      ImportPayment.import parse credit_row
-      expect { ImportPayment.import parse credit_row }
-        .to raise_error NotIdempotent
-    end
-
-    context 'two payments same day' do
-      def payment_later_in_day
-        %q[89, GR, 2012-03-25 14:01:00, Payment Ground Rent, 0, 20, 0]
-      end
-
-      it 'import two credits on one debit' do
-        Credit.any_instance.stub(:type).and_return 'Ground Rent'
-        (property_with_unpaid_debit human_ref: 89).save!
-        ImportPayment.import parse credit_row
-        ImportPayment.import parse payment_later_in_day
-        expect(Payment.all).to have(2).items
-        expect(Credit.all).to have(2).items
-      end
-    end
-
     context 'One credit' do
       def one_credit_csv
         %q[89, GR, 2012-01-11 15:32:00, Payment Gro...,    0, 37.5,    0]
@@ -56,6 +24,25 @@ module DB
         property_create! human_ref: 89
         expect { ImportPayment.import parse one_credit_csv }
           .to change(Credit, :count).by 1
+      end
+    end
+
+    context 'errors' do
+      it 'without charge_type raises error' do
+        Credit.any_instance.stub(:type).and_return 'Service Charge'
+        (property_with_unpaid_debit human_ref: 89).save!
+
+        expect { ImportPayment.import parse credit_row }
+          .to raise_error DB::ChargeTypeUnknown
+      end
+
+      it 'double import raises error' do
+        Credit.any_instance.stub(:type).and_return 'Ground Rent'
+        (property_with_unpaid_debit human_ref: 89).save!
+
+        ImportPayment.import parse credit_row
+        expect { ImportPayment.import parse credit_row }
+          .to raise_error NotIdempotent
       end
     end
 
