9b2150eb60ed5e543a1656a6163cc910f76062e9
Richard Wigley <Richard.Wigley@gmail.com>
2013-12-02 14:35:24 +0000

    FEATURE Advanced credits removing making it special (ongoing)

---
+++ app/decorators/credit_decorator.rb
@@ -0,0 +1,29 @@
+require_relative '../../lib/modules/method_missing'
+
+class CreditDecorator
+  include ActionView::Helpers::NumberHelper
+  include MethodMissing
+  attr_reader :source
+
+  def initialize credit
+    @source = credit
+  end
+
+  def prepare_for_form
+    self.amount = pay_off_debit if amount.blank?
+  end
+
+  def amount
+    number_with_precision(@source.amount,precision: 2)
+  end
+
+  private
+
+  def pay_off_debit
+    @source.pay_off_debit
+  end
+
+  def debit
+    @source.debit
+  end
+end
--- app/decorators/payment_decorator.rb
+++ app/decorators/payment_decorator.rb
@@ -49,7 +49,7 @@ class PaymentDecorator
   end
 
   def generate_credits
-    @credits.push(*@source.credits.map { |d| CreditWithDebitDecorator.new d })
+    @credits.push(*@source.credits.map { |d| CreditDecorator.new d })
     @credits.each { |d| d.prepare_for_form }
     @credits
   end
