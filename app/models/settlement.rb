bf1bf71c7b4d2be3cca0f98df5cf826f2bc8123c
Richard Wigley <Richard.Wigley@gmail.com>
2014-01-11 12:45:40 +0000

    ADVANCED PAYMENTS settlements use 'amount not used' and not amount

--- app/models/settlement.rb
+++ app/models/settlement.rb
@@ -5,17 +5,19 @@ class Settlement < ActiveRecord::Base
   validates :credit_id, :debit_id, :amount, presence: true
 
   def self.resolve_credit credit, debits
-    owe = credit.amount
+    cover = credit.spendable
     debits.each do |debit|
-      owe -= pay = [owe, debit.amount].min
+      cover -= pay = [cover, debit.outstanding].min
+      break if pay.round(2) == 0.00
       credit.settlements.build debit: debit, amount: pay
     end
   end
 
   def self.resolve_debit debit, credits
-    owe = debit.amount
+    owe = debit.outstanding
     credits.each do |credit|
-      owe -= pay = [owe, credit.amount].min
+      owe -= pay = [owe, credit.spendable].min
+      break if pay.round(2) == 0.00
       debit.settlements.build credit: credit, amount: pay
     end
   end
