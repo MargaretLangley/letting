8cbbeca3a285ffed5c69ec3e61bf69dc4c37c22b
Richard Wigley <Richard.Wigley@gmail.com>
2014-01-27 11:35:39 +0000

    REMOVE FEATURE BLOCKS

--- app/models/block.rb
+++
@@ -1,3 +0,0 @@
-class Block < ActiveRecord::Base
-  validates :name, length: { maximum: 64 }, presence: true
-end
--- db/migrate/20130805170505_create_blocks.rb
+++
@@ -1,9 +0,0 @@
-class CreateBlocks < ActiveRecord::Migration
-  def change
-    create_table :blocks do |t|
-      t.string :name, null: false
-
-      t.timestamps
-    end
-  end
-end
