29fb5884a5e48aaf97f7b051d841a7a50802be9c
Richard Wigley <Richard.Wigley@gmail.com>
2014-02-03 16:43:22 +0000

    import_client tests from using csv to using stirng in spec

--- spec/lib/import/import_client_spec.rb
+++ spec/lib/import/import_client_spec.rb
@@ -6,32 +6,52 @@ require_relative '../../../lib/import/import_client'
 
 module DB
   describe ImportClient, :import do
+
+    def row
+      %q[11,  Mr,  D, Example, Mrs, A N, Other, 1, ExampleHouse,  2, ] +
+      %q[Example Street, ,Example Town,  Example County,  E10 7EX]
+    end
+
+    def updated_row
+      %q[11,  Mr,  E, Changed, Mrs, A N, Other, 1, ExampleHouse,  2, ] +
+      %q[Example Street, Example District,Example Town,  Example County,  E10 7EX]
+    end
+
+    def one_entity_row
+      %q[11,  Mr,  D, Example, , , , 1, ExampleHouse,  2, ] +
+      %q[Example Street, ,Example Town,  Example County,  E10 7EX]
+    end
+
+    def initialised_company_row
+      %q[11,  ,  A D, Example Ltd, , , , 1, ExampleHouse,  2, ] +
+      %q[Example Street, ,Example Town,  Example County,  E10 7EX]
+    end
+
     it 'One row' do
-      expect { ImportClient.import client_csv }.to change(Client, :count).by 1
+      expect { import_client row }.to change(Client, :count).by 1
     end
 
     it 'One row, 2 Entities' do
-      expect { ImportClient.import client_csv }.to change(Entity, :count).by 2
+      expect { import_client row }.to change(Entity, :count).by 2
     end
 
     it 'Not double import' do
-      ImportClient.import client_csv
-      expect { ImportClient.import client_csv }.to_not change(Client, :count)
+      import_client row
+      expect { import_client row }.to_not change(Client, :count)
     end
 
     it 'Not double import' do
-      expect { ImportClient.import client_csv }.to change(Entity, :count).by 2
-      expect { ImportClient.import client_csv }.to_not change(Entity, :count)
+      expect { import_client row }.to change(Entity, :count).by 2
+      expect { import_client row }.to_not change(Entity, :count)
     end
 
     context 'entities' do
       it 'adds one entity when second entity blank' do
-        expect { ImportClient.import client_one_entity_csv }
-          .to change(Entity, :count).by 1
+        expect { import_client one_entity_row }.to change(Entity, :count).by 1
       end
 
       it 'ordered by creation' do
-        ImportClient.import ImportClient.import client_csv
+        import_client row
         expect(Client.first.entities[0].created_at).to be <
           Client.first.entities[1].created_at
       end
@@ -39,49 +59,36 @@ module DB
       context 'multiple imports' do
 
         it 'updated changed entities' do
-          ImportClient.import client_csv
-          ImportClient.import client_updated_csv
+          import_client row
+          import_client updated_row
           expect(Client.first.entities[0].name).to eq 'Changed'
           expect(Client.first.entities[1].name).to eq 'Other'
         end
 
         it 'removes deleted second entities' do
-          ImportClient.import client_csv
-          expect { ImportClient.import client_one_entity_csv }
-            .to change(Entity, :count).by(-1)
+          import_client row
+          expect { import_client one_entity_row }.to change(Entity, :count).by(-1)
         end
       end
 
       context 'In odd state' do
         it 'concats a company with intials' do
-          ImportClient.import client_company_with_intials_csv
+          import_client initialised_company_row
           expect(Entity.first.name).to eq 'A D Example Ltd'
         end
       end
     end
 
-    def client_csv
-      FileImport.to_a('clients',
-                      headers: FileHeader.client,
-                      location: 'spec/fixtures/import_data/clients')
+    def import_client row, args = {}
+      ImportClient.import parse(row), args
     end
 
-    def client_updated_csv
-      FileImport.to_a 'clients_updated',
-                      headers: FileHeader.client,
-                      location: 'spec/fixtures/import_data/clients'
-    end
-
-    def client_one_entity_csv
-      FileImport.to_a 'clients_one_entity',
-                      headers: FileHeader.client,
-                      location: 'spec/fixtures/import_data/clients'
-    end
-
-    def client_company_with_intials_csv
-      FileImport.to_a 'clients_company_with_initials',
-                      headers: FileHeader.client,
-                      location: 'spec/fixtures/import_data/clients'
+    def parse row_string
+      CSV.parse(row_string,
+                headers: FileHeader.client,
+                header_converters: :symbol,
+                converters: -> (f) { f ? f.strip : nil }
+               )
     end
   end
 end
