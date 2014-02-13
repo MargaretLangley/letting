a5358a2ad45c9a30f85ba6b19375cb440eed5b64
Richard Wigley <Richard.Wigley@gmail.com>
2014-02-13 09:51:15 +0000

    SPEC MINOR

--- spec/decorators/account_credit_decorator_spec.rb
+++ spec/decorators/account_credit_decorator_spec.rb
@@ -4,25 +4,25 @@ describe AccountCreditDecorator do
 
   let(:credit_dec) { AccountCreditDecorator.new credit_new }
 
-  context 'attributes has' do
-    it 'charge_type' do
+  describe 'accessors for' do
+    it '#charge_type' do
       credit_dec.charge = Charge.new charge_attributes
       expect(credit_dec.charge_type).to eq 'Ground Rent'
     end
 
-    it 'date' do
+    it '#date' do
       expect(credit_dec.on_date).to eq Date.new 2013, 4, 30
     end
 
-    it 'due' do
+    it '#due' do
       expect(credit_dec.due).to eq ''
     end
 
-    it 'payment' do
+    it '#payment' do
       expect(credit_dec.payment).to eq '88.08'
     end
 
-    it 'amount' do
+    it '#amount' do
       expect(credit_dec.amount).to eq(-88.08)
     end
   end
