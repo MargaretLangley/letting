aa0f429e0e250cb8d277e8d7b68cadcd92866c8a
Richard Wigley <Richard.Wigley@gmail.com>
2014-02-15 12:33:49 +0000

    debit_generator REFACTORED tests

--- spec/features/debit_generator/debit_generator_create_spec.rb
+++ spec/features/debit_generator/debit_generator_create_spec.rb
@@ -4,46 +4,31 @@ describe 'debit_generator' do
   let(:debit_gen_page) { DebitGeneratorCreatePage.new }
   before(:each) { log_in }
 
-  it 'Errors' do
-    debit_gen_page.visit_page.search_term('Garbage').search
-    expect(current_path).to eq '/debit_generators'
-    expect(debit_gen_page).to be_without_accounts
-  end
-
-  context 'creates' do
+  describe '#create' do
     before { Timecop.travel(Date.new(2013, 1, 31)) }
     after  { Timecop.return }
 
-
-    it 'no invoices found' do
-      property_with_charge_create!
-      debit_gen_page.visit_page
-      debit_gen_page.search_term('102-109').search
-      expect(page).to have_text 'The invoicing could not be saved'
+    it 'errors without a valid account' do
+      property_with_charge_create! human_ref: 99
+      debit_gen_page.visit_page.search_term('102-109').search
+      expect(debit_gen_page).to be_without_accounts
     end
 
-    it 'reaches make charges' do
-      property_with_charge_create!
-      debit_gen_page.visit_page
-      debit_gen_page.search_term('Hillbank House').search
-      expect(page).to have_text '2002'
-      expect(page).to have_text 'Ground Rent'
-      debit_gen_page.make_charges
-    end
-
-    it 'debits' do
-      property = property_with_charge_create!
-      property.client = client_create! human_ref: 8989
+    it 'charges a property that matches the search' do
+      property = property_with_charge_create! \
+                   human_ref: 2002,
+                   address_attributes: { road: 'Talbot Road' }
+      # Client required because controller starts invoicing immediately
+      # Be nice to disconnect this requirement.
+      property.client = client_create!
       property.save!
-      debit_gen_page.visit_page
-      debit_gen_page.search_term('Hillbank House').search
+      debit_gen_page.visit_page.search_term('Talbot Road').search
       expect(page).to have_text '2002'
       expect(page).to have_text 'Ground Rent'
       debit_gen_page.make_charges
+      save_and_open_page
       expect(debit_gen_page).to be_created
     end
-
-
   end
 
 end
