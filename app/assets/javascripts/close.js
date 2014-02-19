26c140a1534809bb69a14f42806c6940b2a4c3e1
MargaretLangley <margaret.b.langley@gmail.com>
2014-02-19 11:34:52 +0000

    FLASH both timeout and click available in close.js

--- app/assets/javascripts/close.js
+++ app/assets/javascripts/close.js
@@ -1,5 +1,13 @@
+$( document ).ready(function() {
+
+  $('.js-close-click').click(function(event) {
+    $(this).slideToggle('fast');
+  });
+});
+
 function readyFn( jQuery ) {
-  setTimeout(function () { $('.js-close').slideToggle('fast'); }, 2000);
+  setTimeout(function () { $('.js-close-time').slideToggle('fast'); }, 2000);
 }
 
 $( document ).ready( readyFn );
+
