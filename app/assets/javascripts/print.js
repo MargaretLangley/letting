cb8871d6d4bb24d16ba7d02e5997e7a2b775d159
margaret <margaret.b.langley@gmail.com>
2014-07-23 13:44:55 +0000

    print-window updated to js-print-window

--- app/assets/javascripts/print.js
+++ app/assets/javascripts/print.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
- // print window
-  $('.print-window').click(function(event) {
+ // js print window
+  $('.js-print-window').click(function(event) {
     window.print();
   });
 });
