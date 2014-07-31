d1da4898b1700948e7d7b6910d05c19f87c99549
Richard Wigley <richard.wigley@gmail.com>
2014-07-31 11:02:30 +0000

    amount_validator documented and refactored

--- app/validators/amount_validator.rb
+++ app/validators/amount_validator.rb
@@ -4,15 +4,27 @@
 #
 # Validates that the amount is valid
 #
+# Usage
+#
+# class Balance
+#   validates :amount, amount: true
+# end
+#
+# Further Reading http://api.rubyonrails.org/v4.1.1/classes/ActiveModel/Validator.html
+#
 # Params are passed to classes as strings. However, by the time they
 # are validated they have been parsed and the value is a decimal.
 #
 ####
 #
 class AmountValidator < ActiveModel::EachValidator
+
+  # if the validation fails we add an error to the record for this attribute.
+  #
   def validate_each(record, attribute, value)
-    return if value && value.to_s =~ /\A\d+??(?:\.\d{0,})?\z/ && value >= 0
-    record.errors[attribute] << error_message(value)
+    return if value && value.to_s =~ /\A\d+??(?:\.\d{0,})?\z/
+
+    record.errors.add attribute, error_message(value)
   end
 
   def error_message value
