661e353af3a333139db859ad6e92f74cee8bbe8a
margaret <margaret.b.langley@gmail.com>
2014-08-09 04:58:10 +0100

    Notice update

--- app/assets/javascripts/close.js
+++ app/assets/javascripts/close.js
@@ -9,5 +9,10 @@ function readyFn( jQuery ) {
   setTimeout(function () { $('.js-close-time').slideToggle('fast'); }, 2000);
 }
 
+function slowFn( jQuery ) {
+  setTimeout(function () { $('.js-slow-time').slideToggle('slow'); }, 9000);
+}
+
 $( document ).ready( readyFn );
+$( document ).ready( slowFn );
 
