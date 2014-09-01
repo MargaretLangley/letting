aab60c5a12333a0d7f865681dfbe26fe5894ba45
Richard Wigley <richard.wigley@gmail.com>
2014-09-01 21:24:06 +0100

    sheet factory moved from spec/factories =>  support/factories

--- spec/factories/sheet_factory_spec.rb
+++ spec/factories/sheet_factory_spec.rb
@@ -1,12 +1,17 @@
-# rubocop: disable Style/ParameterLists
-
 require 'rails_helper'
 
-def sheet_create(id:, description:, invoice_name:, phone:, vat: vat, heading2:)
-  Sheet.create! id: id,
-                description: description,
-                invoice_name: invoice_name,
-                phone: phone,
-                vat: vat,
-                heading2: heading2
+describe 'Sheet Factory' do
+  describe 'sheet_create' do
+    describe 'default' do
+      it 'is valid' do
+        sheet_create id: 1,
+                     description: 'Page 1 Invoice',
+                     invoice_name: 'Morgan',
+                     phone: '01710008',
+                     vat: '89',
+                     heading2: 'give you notice pursuant'
+        expect(Sheet.count).to eq 1
+      end
+    end
+  end
 end
--- spec/support/factories/sheet_factory.rb
+++ spec/support/factories/sheet_factory.rb
@@ -1,12 +1,10 @@
-def sheet_factory **args
-  sheet = Sheet.new sheet_attributes args
-  sheet.build_address address_attributes args.fetch(:address_attributes, {})
-  sheet.save!
-  sheet
-end
+# rubocop: disable Style/ParameterLists
 
-def sheet2_factory **args
-  sheet = Sheet.new sheet_p2_attributes args
-  sheet.save!
-  sheet
+def sheet_create(id:, description:, invoice_name:, phone:, vat:, heading2:)
+  Sheet.create! id: id,
+                description: description,
+                invoice_name: invoice_name,
+                phone: phone,
+                vat: vat,
+                heading2: heading2
 end
