70031b48f0236ced2f292d539e301568b6997ca7
margaret <margaret.b.langley@gmail.com>
2014-09-03 15:56:11 +0100

    Sheet model tests & removal of unused invoice_texts

--- spec/support/factories/sheet_factory.rb
+++ spec/support/factories/sheet_factory.rb
@@ -1,5 +1,17 @@
 # rubocop: disable Style/ParameterLists
 
+def sheet_new id: 4,
+              description: 'Page 1 Invoice',
+              invoice_name: 'Bell',
+              phone: '01710008',
+              vat: '89'
+  Sheet.new id: id,
+            description: description,
+            invoice_name: invoice_name,
+            phone: phone,
+            vat: vat
+end
+
 def sheet_create(id:, description:, invoice_name:, phone:, vat:, heading2:)
   Sheet.create! id: id,
                 description: description,
