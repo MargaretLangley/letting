852d7c90006c3f61fd308591b9e4774e8c5c1e6e
margaret <margaret.b.langley@gmail.com>
2014-09-14 22:12:59 +0100

    Sheets, text for invoices, form and tests improved

--- spec/models/invoices/sheet_spec.rb
+++ spec/models/invoices/sheet_spec.rb
@@ -3,20 +3,9 @@ require 'rails_helper'
 describe Sheet, type: :model do
 
   describe 'validations' do
-    it('is valid') do
-      expect(sheet_new).to be_valid
-    end
-
-    it 'requires a description' do
-      expect(sheet_new invoice_name: '').to_not be_valid
-    end
-
-    it 'requires a phone' do
-      expect(sheet_new phone: '').to_not be_valid
-    end
-
-    it 'requires vat' do
-      expect(sheet_new vat: '').to_not be_valid
-    end
+    it('returns valid') { expect(sheet_new).to be_valid }
+    it('requires a name') { expect(sheet_new invoice_name: '').to_not be_valid }
+    it('requires a phone') { expect(sheet_new phone: '').to_not be_valid }
+    it('requires a vat number') { expect(sheet_new vat: '').to_not be_valid }
   end
 end
