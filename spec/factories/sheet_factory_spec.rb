11784392e9d268dc577f975f774c9b739f3d662a
Richard Wigley <richard.wigley@gmail.com>
2014-09-16 10:51:01 +0100

    sheet factory added specs - makes address. debit_generator required sheet to have an address

--- spec/factories/sheet_factory_spec.rb
+++ spec/factories/sheet_factory_spec.rb
@@ -1,13 +1,43 @@
 require 'rails_helper'
 
 describe 'Sheet Factory' do
-  it 'is valid' do
-    sheet_create id: 1,
-                 description: 'Page 1 Invoice',
-                 invoice_name: 'Morgan',
-                 phone: '01710008',
-                 vat: '89',
-                 heading2: 'give you notice pursuant'
-    expect(Sheet.count).to eq 1
+  describe 'new' do
+    # We create the object and test it has expected value
+    describe 'default' do
+      it 'has description' do
+        expect(sheet_new.description).to eq 'Page 1 Invoice'
+      end
+      describe 'makes' do
+        it 'has address' do
+          expect(sheet_new.address).to_not be_nil
+        end
+      end
+    end
+    describe 'adds' do
+      # adds notice..
+    end
+    describe 'overrides' do
+      it 'changes description' do
+        expect(sheet_new(description: 'new').description).to eq 'new'
+      end
+
+      it 'changes address' do
+        expect(sheet_new(address: address_new(road: 'new')).address.road)
+          .to eq 'new'
+      end
+    end
+  end
+
+  describe 'create' do
+    describe 'default' do
+      it 'is valid' do
+        expect { sheet_create }.to change(Sheet, :count).by(1)
+      end
+      describe 'makes' do
+        it 'builds address' do
+          expect(sheet_create.address).to_not be_nil
+        end
+      end
+    end
   end
 end
--- spec/support/factories/sheet_factory.rb
+++ spec/support/factories/sheet_factory.rb
@@ -1,22 +1,34 @@
 # rubocop: disable Metrics/ParameterLists
+# rubocop: disable Metrics/MethodLength
 
-def sheet_new id: 4,
+def sheet_new id: nil,
               description: 'Page 1 Invoice',
               invoice_name: 'Bell',
               phone: '01710008',
-              vat: '89'
+              vat: '89',
+              heading2: nil,
+              address: address_new
   Sheet.new id: id,
             description: description,
             invoice_name: invoice_name,
             phone: phone,
-            vat: vat
+            vat: vat,
+            heading2: heading2,
+            address: address
 end
 
-def sheet_create(id:, description:, invoice_name:, phone:, vat:, heading2:)
+def sheet_create(id: nil,
+                 description: 'Page 1 Invoice',
+                 invoice_name: 'Bell',
+                 phone: '01710008',
+                 vat: '89',
+                 heading2: nil,
+                 address: address_new)
   Sheet.create! id: id,
                 description: description,
                 invoice_name: invoice_name,
                 phone: phone,
                 vat: vat,
-                heading2: heading2
+                heading2: heading2,
+                address: address
 end
