ea8587e02794ce1d35141e0daf0834f7d2e006a7
Richard Wigley <richard.wigley@gmail.com>
2014-09-18 13:23:59 +0100

    routes organised

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,26 @@
 Letting::Application.routes.draw do
 
+  resources :sessions, only: [:create, :destroy]
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   get '/404', to: 'errors#not_found'
   get '/422', to: 'errors#server_error'
   get '/500', to: 'errors#server_error'
 
-  resources :users
-  resources :sessions, only: [:create, :destroy]
-
   get 'search' => 'search#index', as: :search
+  # TODO: name change to autocomplete?
+  resources :search_suggestions, only: [:index]
 
   root 'properties#index'
   resources :properties
-  resources :search_suggestions, only: [:index]
   resources :clients
-
-  resources :charges
+  # TODO: do we need debit_generators?
   resources :debit_generators, only: [:new, :create, :index]
-
-  resources :charge_cycles
-
   resources :payments
   resources :invoices, only: [:index, :show]
+
+  # Admin
+  resources :charge_cycles
+  resources :users
   resources :sheets, only: [:index, :show, :edit, :update]
 end
