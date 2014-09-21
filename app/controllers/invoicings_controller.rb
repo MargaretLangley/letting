a40f2e687353d0a680ceb0d7954ccca9bbba79e4
Richard Wigley <richard.wigley@gmail.com>
2014-09-21 18:10:21 +0100

    invoicing controller new/create, new.html displays invoicing and basic invoice information

--- app/controllers/invoicings_controller.rb
+++ app/controllers/invoicings_controller.rb
@@ -8,11 +8,26 @@
 #
 class InvoicingsController < ApplicationController
   def new
-    @invoicing = Invoicing.new start_date: params[:start_date],
+    @invoicing = Invoicing.new property_range: params[:search_terms],
+                               start_date: params[:start_date],
                                end_date: params[:end_date]
     @invoicing.generate account_ids: params[:id] if params[:id]
   end
 
   def create
+    @invoicing = Invoicing.new invoicing_params
+    if @invoicing.save
+      redirect_to new_invoicing_path, notice: 'Invoicing successfully created'
+    else
+      render :new
+    end
+  end
+
+
+  def invoicing_params
+    params.require(:invoicing)
+      .permit :property_range,
+              :start_date,
+              :end_date
   end
 end
--- app/models/invoicing.rb
+++ app/models/invoicing.rb
@@ -9,7 +9,6 @@ class Invoicing < ActiveRecord::Base
     end
   end
 
-
   def properties_range accounts
     "#{accounts.first.property.human_ref} - #{accounts.last.property.human_ref}"
   end
