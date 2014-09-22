4833fe79df4d85a4e2d3aa125fbe299f2bb24770
margaret <margaret.b.langley@gmail.com>
2014-09-22 14:15:36 +0100

    Notices verification, name changes, migration & schema update

--- spec/support/factories/notice_factory.rb
+++ spec/support/factories/notice_factory.rb
@@ -1,19 +1,19 @@
 def notice_new id: nil,
                instruction: 'ins1',
-               clause: 'This is useful',
-               proxy: 'Filled'
+               fill_in: 'This is useful',
+               sample: 'Filled'
   Notice.new id: id,
              instruction: instruction,
-             clause: clause,
-             proxy: proxy
+             fill_in: fill_in,
+             sample: sample
 end
 
 def notice_create id: nil,
                   instruction: 'ins1',
-                  clause: 'This is useful',
-                  proxy: 'Filled'
+                  fill_in: 'This is useful',
+                  sample: 'Filled'
   Notice.create! id: id,
                  instruction: instruction,
-                 clause: clause,
-                 proxy: proxy
+                 fill_in: fill_in,
+                 sample: sample
 end
