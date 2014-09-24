d764643a66f252fc46745b7d592e65b0ecff8837
Richard Wigley <richard.wigley@gmail.com>
2014-09-24 13:02:04 +0100

    invoicing.rb specs for validations

--- spec/models/invoicing_spec.rb
+++ spec/models/invoicing_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Invoicing, type: :model do
+  it('is valid') { expect(invoicing_new).to be_valid }
+  describe 'validates presence' do
+    it 'property_range' do
+      expect(invoicing_new property_range: nil).to_not be_valid
+    end
+    it('start_date') { expect(invoicing_new start_date: nil).to_not be_valid }
+    it('end_date') { expect(invoicing_new end_date: nil).to_not be_valid }
+    it('invoices') { expect(invoicing_new invoices: nil).to_not be_valid }
+  end
 
   describe '#invoicable_accounts' do
     before { Timecop.travel(Date.new(2014, 6, 1)) }
