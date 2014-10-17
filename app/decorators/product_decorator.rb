3e4e590ce6b2e34f34a792c7dcaf5ed7599e9af8
Richard Wigley <richard.wigley@gmail.com>
2014-10-17 12:12:18 +0100

    product using decorator

---
+++ app/decorators/product_decorator.rb
@@ -0,0 +1,35 @@
+###
+# ProductDecorator
+#
+# Adds display logic to the charge business object.
+##
+#
+class ProductDecorator
+  include MethodMissing
+
+  def product
+    @source
+  end
+
+  def initialize source_product
+    @source = source_product
+  end
+
+  def date_due
+    I18n.l product.date_due, format: :short
+  end
+
+  def period
+    return '' unless product.period.first && product.period.last
+    "#{I18n.l product.period.first, format: :short} to "\
+    "#{I18n.l product.period.last, format: :short}"
+  end
+
+  def amount
+    number_with_precision(product.amount, precision: 2)
+  end
+
+  def total
+    number_with_precision(total, precision: 2)
+  end
+end
---
+++ spec/decorators/product_decorator_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe ProductDecorator, :invoice do
+  describe 'methods' do
+    it 'returns #date_due formatted' do
+      product_dec = ProductDecorator.new product_new date_due: '2014-06-07'
+      expect(product_dec.date_due).to eq '07/Jun/14'
+    end
+
+    it 'returns #period formatted' do
+      product_dec = ProductDecorator.new product_new period: Date.new(2010, 9, 30)..
+                                                             Date.new(2011, 3, 25)
+      expect(product_dec.period).to eq '30/Sep/10 to 25/Mar/11'
+    end
+
+    it 'returns empty string if #period range nil' do
+      product_dec = ProductDecorator.new product_new period: nil..nil
+      expect(product_dec.period).to eq ''
+    end
+  end
+end
