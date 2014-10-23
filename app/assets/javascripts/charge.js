65d078710fcc8d75cdbe7b2359917c459a1c1436
Richard Wigley <richard.wigley@gmail.com>
2014-10-23 15:50:09 +0100

    comment for charge.js - added expected html into comment

--- app/assets/javascripts/charge.js
+++ app/assets/javascripts/charge.js
@@ -1,15 +1,48 @@
-// #
-// # Dynamic Select Box
-// #
-// # http://railscasts.com/episodes/88-dynamic-select-menus-revised?view=asciicast
-// #
-// # Select boxes using class identifier instead of id (as we need) more than one
-// # selected box on the same form.
-// # js-dynamic-group - outer box containing the related select boxes.
-// # js-dynamic-filter - selecting drop-down filters the results
-// # js-dynamic-result - the drop-down whose results are reduced by the filtering.
-// #
-// #
+//
+// Dynamic Select Box
+//
+// http://railscasts.com/episodes/88-dynamic-select-menus-revised?view=asciicast
+//
+// Select boxes using class identifier instead of id (as we need) more than one
+// selected box on the same form.
+// js-dynamic-group - outer box containing the related select boxes.
+// js-dynamic-filter - selecting drop-down filters the results
+// js-dynamic-result - the drop-down whose results are reduced by the filtering.
+//
+// How the select appears in html.
+//
+// js-dynamic-filter
+//
+// <select class="js-dynamic-filter" name="property[account_attributes][charges_attributes][0][cycle_id]">
+// <option value="1">Mar 25th / Sep 29th</option>
+// <option selected="selected" value="4">Jun 24th / Dec 25th</option>
+// <option value="4">Jun 25th / Dec 25th</option>
+// </select>
+//
+// js-dynamic-result
+//
+// <select class="js-dynamic-result" name="property[account_attributes][charges_attributes][0][charged_in_id]">
+//   <option value=""></option>
+//   <optgroup label="Mar 25th / Sep 29th">
+//     <option selected="selected" value="1">Arrears</option>
+//     <option value="2">Advance</option>
+//     <option value="3">Mid-Term</option>
+//   </optgroup>
+//   <optgroup label="Jun 24th / Dec 25th">
+//     <option selected="selected" value="1">Arrears</option>
+//     <option value="2">Advance</option>
+//   </optgroup>
+//   <optgroup label="Jun 25th / Dec 25th">
+//     <option value="2">Advance</option>
+//   </optgroup>
+// </select>
+//
+// updatepayment options is updated each time js-dynamic filter is changed
+// in this case when Mar 25th / Sep 29th is selected the js-dynamic-result becomes:
+//
+// <option selected="selected" value="1">Arrears</option>
+// <option value="2">Advance</option>
+// <option value="3">Mid-Term</option>
 
 // ready - executes when HTML-Document is loaded and DOM is ready
 $( document ).ready(function() {
