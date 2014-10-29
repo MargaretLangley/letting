20490dbf4535f1748e387c5796afa73022352bc3
Richard Wigley <richard.wigley@gmail.com>
2014-10-29 16:23:12 +0000

    SearchController refactored - TODO removed

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,46 +9,62 @@
 # Literal controller is responsible for exact matches.
 # FullTextSearch is responsible for the fuzzy searching.
 #
-# rubocop: disable Lint/AssignmentInCondition
-# REFACTOR index TODO:
-# rubocop: disable  Metrics/MethodLength
+# rubocop: disable Lint/AssignmentInCondition, Style/AccessorMethodName
 #
 ####
 #
 class SearchController < ApplicationController
   def index
-    session[:search_model] = referer_model unless referer_model == 'Search'
-    match = LiteralSearch.search(type:  session[:search_model],
-                                 query: params[:search_terms]).go
-    if match[:record_id] || match[:process_empty] == 'true'
-      redirect_to controller: match[:controller],
-                  action: match[:action],
-                  id: match[:record_id],
-                  search_terms: params[:search_terms],
-                  start_date: params[:start_date],
-                  end_date: params[:end_date]
+    session[:search_model] = referer unless referer == 'Search'
+    if literal_search_hit? literal_search
+      redirect_to redirect_params(literal_search).merge(repack_search_params)
     else
-      results = full_text_search search_model: session[:search_model],
-                                 query: params[:search_terms]
-      @records = results[:records].page(params[:page])
-      render results[:render]
+      @records = full_text_search[:records].page(params[:page])
+      render full_text_search[:render]
     end
   end
 
   private
 
-  def full_text_search(search_model:, query:)
-    results = FullTextSearch.search(type:  search_model, query: query).go
-    flash_no_results if results[:success] == false && query.present?
-    results
-  end
-
-  def referer_model
+  def referer
     (Rails.application.routes.recognize_path(request.referrer)[:controller])
       .classify
   end
 
-  def flash_no_results
-    flash.now[:alert] = 'No Matches found. Search again.'
+  def literal_search_hit? match
+    match[:record_id] || match[:process_empty] == 'true'
+  end
+
+  def literal_search
+    @literal_search ||= LiteralSearch.search(type:  session[:search_model],
+                                             query: params[:search_terms]).go
+  end
+
+  def redirect_params match
+    {
+      controller:   match[:controller],
+      action:       match[:action],
+      id:           match[:record_id],
+    }
+  end
+
+  def repack_search_params
+    {
+      search_terms: params[:search_terms],
+      start_date:   params[:start_date],
+      end_date:     params[:end_date]
+    }
+  end
+
+  def full_text_search
+    @full_text_search ||= get_full_text_search
+  end
+
+  def get_full_text_search
+    results = FullTextSearch.search(type: session[:search_model],
+                                    query: params[:search_terms]).go
+    flash.now[:alert] = 'No Matches found. Search again.' \
+      if results[:success] == false && params[:search_terms].present?
+    results
   end
 end
