f4efb3bf6a38306fec6387b5947d8edcbb72620a
Richard Wigley <richard.wigley@gmail.com>
2014-10-29 15:21:04 +0000

    Literal_search attributes => accesor methods

--- app/models/utils/literal_search.rb
+++ app/models/utils/literal_search.rb
@@ -2,7 +2,7 @@
 #
 # LiteralSearch
 #
-# Responsible for exact match seraches for the application.
+# Responsible for exact match searches for the application.
 #
 # Used by search Controller
 #
@@ -13,6 +13,7 @@
 ####
 #
 class LiteralSearch
+  attr_reader :type, :query
   def self.search(type:, query:)
     new(type: type, query: query)
   end
@@ -30,46 +31,46 @@ class LiteralSearch
   end
 
   def type_query
-    case @type
+    case type
     when 'Cycle'
       {
         controller: 'cycles',
         action: 'show',
-        record_id: id_or_nil(Cycle.find_by name: @query)
+        record_id: id_or_nil(Cycle.find_by name: query)
       }
     when 'Client'
       {
         controller: 'clients',
         action: 'show',
-        record_id: id_or_nil(Client.find_by human_ref: @query)
+        record_id: id_or_nil(Client.find_by human_ref: query)
       }
     when 'Invoicing'
       {
         controller: 'invoicings',
         action: 'new',
-        record_id: Account.between?(@query).pluck(:id)
+        record_id: Account.between?(query).pluck(:id)
       }
     when 'Payment'
       {
-        process_empty: process_empty(@query),
+        process_empty: process_empty(query),
         controller: 'payments',
         action: 'new',
-        record_id: id_or_nil(Account.find_by_human_ref @query)
+        record_id: id_or_nil(Account.find_by_human_ref query)
       }
     when 'Property'
       {
         controller: 'properties',
         action: 'show',
-        record_id: id_or_nil(Property.find_by human_ref: @query)
+        record_id: id_or_nil(Property.find_by human_ref: query)
       }
     when 'User'
       {
         controller: 'users',
         action: 'show',
-        record_id: id_or_nil(User.find_by nickname: @query)
+        record_id: id_or_nil(User.find_by nickname: query)
       }
     else
-      fail NotImplementedError, "Missing type: #{@type}"
+      fail NotImplementedError, "Missing type: #{type}"
     end
   end
 
@@ -82,10 +83,10 @@ class LiteralSearch
   end
 
   def default_ordered_query
-    record = Property.find_by(human_ref: @query) ||
-      Client.find_by(human_ref: @query) ||
-      User.find_by(nickname: @query) ||
-      Cycle.find_by(name: @query)
+    record = Property.find_by(human_ref: query) ||
+      Client.find_by(human_ref: query) ||
+      User.find_by(nickname: query) ||
+      Cycle.find_by(name: query)
     { record_id: record }
   end
 end
