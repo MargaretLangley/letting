f09db4a5933a79e98d38d4575ddb244919a07628
Richard <richard.wigley@gmail.com>
2014-11-17 12:20:57 +0000

    Index pages, title view added to table-row columns. action icons moved into their own column and removed padding and alignment

--- spec/features/invoicing/invoicing_index_spec.rb
+++ spec/features/invoicing/invoicing_index_spec.rb
@@ -16,18 +16,5 @@ describe Invoicing, type: :feature do
       expect(page).to have_text '1-200'
       expect(page).to have_text '30/Jun/14'
     end
-
-    it 'has view link' do
-      invoicing_create property_range: '1-200',
-                       period_first: '2014/06/30',
-                       period_last: '2014/08/30'
-      visit '/invoicings/'
-
-      first(:link, 'View').click
-      expect(page.title).to eq 'Letting - Invoicing'
-      expect(page).to have_text '1-200'
-      expect(page).to have_text '30/Jun/14'
-    end
-
   end
 end
