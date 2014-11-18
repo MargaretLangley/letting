0dbbf40ddafbe2e9afb43b4cb469b9caf92f7b2f
margaret <margaret.b.langley@gmail.com>
2014-11-18 17:38:40 +0000

    Guide & Template update to no blank items

--- spec/features/guide/guide_update_spec.rb
+++ spec/features/guide/guide_update_spec.rb
@@ -6,28 +6,16 @@ describe Template, type: :feature do
     log_in admin_attributes
   end
 
-  describe '#update page 2' do
-    it 'finds data on 2nd page and succeeds' do
-      template_create id: 2
+  describe '#update page 1' do
+    it 'finds data on 1st page and succeeds' do
+      template_create id: 1
       guide_create instruction: 'ins1'
       guide_create fillin: 'Useful'
       guide_create sample: 'Sample'
-      visit '/templates/2/edit'
-      # expect(page.title). to eq 'Letting - Edit Invoice Text'
-      # click_on 'Update Invoice Text'
-      # expect(page).to have_text /successfully updated!/i
+      visit '/templates/1/edit'
+      expect(page.title). to eq 'Letting - Edit Invoice Text'
+      click_on 'Update Invoice Text'
+      expect(page).to have_text /successfully updated!/i
     end
-  end
-
-  it 'finds data on 2nd page and allows blank' do
-    template_create id: 2
-    guide_create instruction: 'ins2'
-    visit '/templates/2/edit'
-    # expect(page.title).to eq 'Letting - Edit Invoice Text'
-    # guide_create instruction: ''
-    # guide_create fillin: ''
-    # guide_create sample: ''
-    # click_on 'Update Invoice Text'
-    # expect(page).to have_text /successfully updated!/i
   end
 end
