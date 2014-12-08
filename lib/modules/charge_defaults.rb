ad8a421e40b00b41eec143088df6eb23ad4ee1da
Richard Wigley <richard.wigley@gmail.com>
2014-12-08 14:01:48 +0000

    BUGFIX Unknown charge was being added into account_edit@ payment dropdown.

--- lib/modules/charge_defaults.rb
+++ lib/modules/charge_defaults.rb
@@ -7,6 +7,6 @@
 ####
 module ChargeDefaults
   PAYMENT_TYPE = [STANDING_ORDER = 'standing_order',
-                  PAYMENT = 'payment',
-                  UNKNOWN_PAYMENT_TYPE = 'unknown_payment_type']
+                  PAYMENT = 'payment']
+  UNKNOWN_PAYMENT_TYPE = 'unknown_payment_type'
 end
