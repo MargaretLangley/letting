67da3fe4bcf40d4ca9ce59e720b0ed5d470c585d
Richard Wigley <richard.wigley@gmail.com>
2014-12-09 14:40:10 +0000

    InvoiceDecorator added trivial accessor added a default to earliest_date_due if none available

--- app/decorators/invoice_decorator.rb
+++ app/decorators/invoice_decorator.rb
@@ -8,41 +8,50 @@ require_relative '../../lib/modules/method_missing'
 # Used when the property has need for behaviour outside of the core
 # of the model. Specifically for display information.
 #
+# rubocop: disable Style/TrivialAccessors
+#
 ####
 #
 class InvoiceDecorator
   include ActionView::Helpers::NumberHelper
   include MethodMissing
-  attr_reader :source
+
+  def invoice
+    @source
+  end
 
   def initialize invoice
     @source = invoice
   end
 
   def invoice_date
-    I18n.l @source.invoice_date, format: :short
+    I18n.l invoice.invoice_date, format: :short
   end
 
   def property_address_one_line
-    @source.property_address.gsub("\n", ', ')
+    invoice.property_address.gsub("\n", ', ')
   end
 
   def billing_agent
-    @source.billing_address.lines.first
+    invoice.billing_address.lines.first
   end
 
   def billing_first_address_line
-    @source.billing_address.lines.second
+    invoice.billing_address.lines.second
   end
 
   def products_display
-    @source.products.first(2).map do |product|
+    invoice.products.first(2).map do |product|
       "#{product.charge_type} "\
         "£#{number_with_precision(product.amount, precision: 2)}"
     end.join ', '
   end
 
   def earliest_date_due
-    I18n.l @source.earliest_date_due, format: :short
+    if invoice.earliest_date_due
+      I18n.l invoice.earliest_date_due, format: :short
+    else
+      invoice_date
+    end
   end
 end
--- spec/decorators/invoice_decorator_spec.rb
+++ spec/decorators/invoice_decorator_spec.rb
@@ -29,4 +29,19 @@ describe InvoiceDecorator do
     invoice_dec = InvoiceDecorator.new invoice_new
     expect(invoice_dec.billing_first_address_line).to eq "Edgbaston Road\n"
   end
+
+  describe '#earliest_date_due' do
+    it 'set to product due_date if available' do
+      product = product_new date_due: Date.new(2010, 3, 25)
+      invoice_dec = InvoiceDecorator.new invoice_new invoice_date: '2000/1/1',
+                                                     products: [product]
+      expect(invoice_dec.earliest_date_due).to eq '25/Mar/10'
+    end
+
+    it 'sets it to invoice_date if no products available' do
+      invoice_dec = InvoiceDecorator.new invoice_new invoice_date: '2000/1/1',
+                                                     products: []
+      expect(invoice_dec.earliest_date_due).to eq '01/Jan/00'
+    end
+  end
 end
