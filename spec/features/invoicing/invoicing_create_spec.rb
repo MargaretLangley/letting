4afb1ee90299e8a0eda7f4be2b870b6cbb54397f
Richard Wigley <richard.wigley@gmail.com>
2014-12-18 14:30:23 +0000

    disable form for editing until a range of properties has been selected

--- spec/features/invoicing/invoicing_create_spec.rb
+++ spec/features/invoicing/invoicing_create_spec.rb
@@ -30,6 +30,11 @@ class InvoicingPage
     self
   end
 
+  def form
+    # fieldset id
+    find('#invoicing')
+  end
+
   def create
     click_on 'Create Invoicing'
     self
@@ -48,39 +53,49 @@ describe Invoicing, type: :feature do
   let(:invoicing_page) { InvoicingPage.new }
   before do
     log_in
+    Timecop.travel Date.new(2013, 6, 1)
   end
   after  { Timecop.return }
 
-  it 'invoices an account that matches the search' do
-    Timecop.travel Date.new(2013, 6, 1)
-    cycle = cycle_new due_ons: [DueOn.new(day: 25, month: 6)]
-    client = client_new
-    account_create property: property_new(human_ref: 87, client: client),
-                   charges: [charge_new(cycle: cycle)]
+  describe 'disabled' do
+    it 'disables fieldset on first visit' do
+      invoicing_page.enter
+      expect(invoicing_page.form).to be_disabled
+    end
 
-    account_create property: property_new(human_ref: 88, client: client),
+    it 'enables fieldset when able to do invoicing' do
+      cycle = cycle_new due_ons: [DueOn.new(day: 25, month: 6)]
+      account_create property: property_new(human_ref: 87, client: client_new),
+                     charges: [charge_new(cycle: cycle)]
+      invoice_text_create id: 1
+
+      invoicing_page.enter
+      invoicing_page.search_term('87').search
+      expect(invoicing_page.form).to_not be_disabled
+    end
+  end
+
+  it 'invoices an account that matches the search' do
+    cycle = cycle_new due_ons: [DueOn.new(day: 25, month: 6)]
+    account_create property: property_new(human_ref: 87, client: client_new),
                    charges: [charge_new(cycle: cycle)]
     invoice_text_create id: 1
 
     invoicing_page.enter
-    invoicing_page.search_term('87-88').search
+
+    invoicing_page.search_term('87').search
     invoicing_page.create
     expect(invoicing_page).to be_success
   end
 
   it 'messages help when no invoicing found' do
-    Timecop.travel Date.new(2013, 7, 1)
-    cycle = cycle_new due_ons: [DueOn.new(day: 25, month: 6)]
-    client = client_new
-    account_create property: property_new(human_ref: 87, client: client),
-                   charges: [charge_new(cycle: cycle)]
-
-    account_create property: property_new(human_ref: 88, client: client),
+    cycle = cycle_new due_ons: [DueOn.new(day: 25, month: 5)]
+    account_create property: property_new(human_ref: 87, client: client_new),
                    charges: [charge_new(cycle: cycle)]
     invoice_text_create id: 1
 
     invoicing_page.enter
-    invoicing_page.search_term('87-88').search
+    invoicing_page.search_term('87').search
     expect(invoicing_page.has_content? /Upcoming Charges/).to be true
   end
 end
