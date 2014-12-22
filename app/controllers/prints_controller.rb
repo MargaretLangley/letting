202fe3554f03055e9f099179c7938feac38d4cf6
margaret <margaret.b.langley@gmail.com>
2014-12-22 19:35:47 +0000

    Cache for debits_transaction, and .deliver used in printing and showing invoices

--- app/controllers/prints_controller.rb
+++ app/controllers/prints_controller.rb
@@ -10,6 +10,6 @@ class PrintsController < ApplicationController
   layout 'print_layout'
 
   def show
-    @run = Run.includes(invoices: [:products]).find params[:id]
+    @run = Run.includes(invoices: [:products, :debits_transaction]).find params[:id]   # rubocop: disable  Metrics/LineLength
   end
 end
