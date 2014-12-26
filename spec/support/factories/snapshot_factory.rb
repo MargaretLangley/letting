e9ca1cff513e6a0ce309422ac59a6d76add5fa9b
Richard Wigley <richard.wigley@gmail.com>
2014-12-26 21:59:36 +0000

    snapshot reverting

--- spec/support/factories/snapshot_factory.rb
+++ spec/support/factories/snapshot_factory.rb
@@ -1,23 +1,5 @@
-def snapshot_new id: nil,
-                 account_id: 1,
-                 period: '2000/01/01'..'2001/03/01',
-                 debits: [debit_new(charge: charge_new)]
-
-  snapshot = Snapshot.new id: id,
-                          account_id: account_id,
-                          period: period
+def snapshot_new id: nil, debits: [debit_new(charge: charge_new)]
+  snapshot = Snapshot.new id: id
   snapshot.debited debits: debits if debits
   snapshot
-end
-
-def snapshot_create id: nil,
-                    account_id: 1,
-                    period: '2000/01/01'..'2001/03/01',
-                    debits: [debit_new(charge: charge_new)]
-
-  snapshot = snapshot_new id: id,
-                          account_id: account_id,
-                          period: period,
-                          debits: debits
-  snapshot.save!
 end
