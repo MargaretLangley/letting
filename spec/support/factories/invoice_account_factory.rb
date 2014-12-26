9d884bde1ec5ab9036fab502ae6927d83736a5db
Richard Wigley <richard.wigley@gmail.com>
2014-12-26 21:42:46 +0000

    invoice_account_factory file name => snapshot_factory  (the names within the file had already changed)

--- spec/support/factories/invoice_account_factory.rb
+++
@@ -1,7 +0,0 @@
-def snapshot_new id: nil,
-                        debits: [debit_new(charge: charge_new)]
-
-  snapshot = Snapshot.new id: id
-  snapshot.debited debits: debits if debits
-  snapshot
-end
