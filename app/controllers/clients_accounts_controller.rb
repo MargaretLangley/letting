7ac9d6c4444a4647c0304bbbab8490121b1970e7
Richard Wigley <richard.wigley@gmail.com>
2014-12-31 01:02:48 +0000

    ClientAccount feature

--- app/controllers/clients_accounts_controller.rb
+++ app/controllers/clients_accounts_controller.rb
@@ -4,7 +4,7 @@
 #
 # Why does this class exist?
 #
-# To give Clients Cround Rent Accounts for both Mar/Sep & June/Dec due dates
+# To give Clients Ground Rent Accounts for both Mar/Sep & June/Dec due dates
 #
 # Client resource covers name, address and properties that the clients
 # own and get payments from.
@@ -12,8 +12,11 @@
 #
 class ClientsAccountsController < ApplicationController
   def show
-    @client = Client.includes(properties: [:address, :account]).find params[:id]
     params[:start_date] ||= Date.new(2014, 1, 1)
     params[:end_date] ||= Time.zone.today
+    @client = Client.includes(properties: [:address, :account]).find params[:id]
+    @accounts = ClientAccount.payments client_id: params[:id],
+                                       start_date: params[:start_date],
+                                       end_date: params[:end_date]
   end
 end
---
+++ app/models/ledgers/client_account.rb
@@ -0,0 +1,24 @@
+class ClientAccount
+  def self.payments(client_id: 1, start_date: '2014-01-01', end_date: '2015-01-01')
+    query = <<-SQL
+      SELECT a.id, a.property_id as property_id, sum(py.amount) * -1 as amount
+      FROM properties as p
+      INNER JOIN addresses as ad ON ad.addressable_id = p.id AND ad.addressable_type = 'Property'
+      INNER JOIN accounts as a ON a.property_id = p.id
+      INNER JOIN payments as py ON py.account_id = a.id
+      INNER JOIN charges as ch ON ch.account_id = a.id
+      INNER JOIN cycles as c ON ch.cycle_id = c.id
+      INNER JOIN due_ons as d1 ON c.id = d1.cycle_Id
+      INNER JOIN due_ons as d2 ON c.id = d1.cycle_Id
+      WHERE p.human_ref < 6000 AND
+            d1.cycle_id = d2.cycle_id AND
+            DUE_ONS_COUNT = 2 AND
+            d1.month = 3 AND d2.month = 9 AND
+            p.client_id = ? AND
+            py.booked_on BETWEEN ? AND ?
+      GROUP BY a.id, p.id, p.human_ref
+      ORDER BY p.human_ref
+    SQL
+    Account.find_by_sql [query, client_id, start_date, end_date]
+  end
+end
