9ddec66def0df9e5a985ffb050640a5cde06862c
Richard Wigley <richard.wigley@gmail.com>
2015-01-03 11:57:06 +0000

    application_helper 2014 => Time.zone.now.year

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -22,8 +22,8 @@ describe ApplicationHelper, type: :helper do
 
   describe '#salient_date_range' do
     it 'includes salient_date interface' do
-      expect(salient_date_range start_date: Date.parse('2014-04-05'),
-                                end_date: Date.parse('2014-06-07'))
+      expect(salient_date_range start_date: Date.new(Time.zone.now.year, 4, 5),
+                                end_date: Date.new(Time.zone.now.year, 6, 7))
         .to eq '05/Apr - 07/Jun'
     end
   end
