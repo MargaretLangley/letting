ce6363154719cd929810b602e21e0e0d38c22e5b
Richard Wigley <richard.wigley@gmail.com>
2015-01-03 12:25:31 +0000

    invoicing_index - hardcoded year broken by new year change. Hardcoded => Time.zone.now.year

--- spec/features/invoicing/invoicing_index_spec.rb
+++ spec/features/invoicing/invoicing_index_spec.rb
@@ -19,8 +19,8 @@ describe Invoicing, type: :feature do
 
     it 'deletes' do
       invoicing_create property_range: '1-200',
-                       period_first: '2014/06/30',
-                       period_last: '2014/08/30'
+                       period_first: "#{Time.zone.now.year}/06/30",
+                       period_last: "#{Time.zone.now.year}/08/30"
       visit '/invoicings/'
 
       expect { click_on 'Delete' }.to change(Invoicing, :count)
