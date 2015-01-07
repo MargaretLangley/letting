9a030520b3d32d76322e6526af561382304f8ab2
Richard Wigley <richard.wigley@gmail.com>
2015-01-07 13:21:00 +0000

    spec refactor

--- spec/models/ledgers/client_payment_spec.rb
+++ spec/models/ledgers/client_payment_spec.rb
@@ -6,7 +6,7 @@ describe ClientPayment, :ledgers do
     expect(payment.years).to eq %w(2015 2014 2013 2012 2011)
   end
 
-  it 'calculates mar range' do
+  it 'calculates #half_year_from' do
     payment = ClientPayment.query(year: 2012)
     expect(payment.half_year_from(month: ClientPayment::MAR))
       .to eq Time.zone.local(2012, 3, 1, 00, 00, 00)..
