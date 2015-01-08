e542bfedf55f8b776763bcae58c9f9c84a146a5a
Richard Wigley <richard.wigley@gmail.com>
2015-01-08 17:25:37 +0000

    ChargePeriod added

---
+++ app/models/ledgers/charge_period.rb
@@ -0,0 +1,38 @@
+class ChargePeriod
+  attr_reader :first, :last
+
+  def initialize(year:, first:, last:)
+    @first = Time.zone.local(year, first, 1, 00, 00)
+    @last = Time.zone.local(year, last, 1, 00, 00)
+  end
+
+  def first_month
+    Date::ABBR_MONTHNAMES[first.month]
+  end
+
+  def last_month
+    Date::ABBR_MONTHNAMES[last.month]
+  end
+
+  def first_month_no
+    first.month
+  end
+
+  def last_month_no
+    last_month_no.month
+  end
+
+  def first_period
+    first..period_length(first)
+  end
+
+  def last_period
+    last..period_length(last)
+  end
+
+  private
+
+  def period_length period
+    period + 6.months
+  end
+end
---
+++ spec/models/ledgers/charge_period_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+describe ChargePeriod, :ledgers do
+end
