bf4364cd9b861c9f8d9a273b8fefabfb5d5f6b92
margaret <margaret.b.langley@gmail.com>
2015-01-22 10:05:27 +0000

    Rename of spec views to shows

--- spec/features/user/user_show_spec.rb
+++ spec/features/user/user_show_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 describe User, type: :feature do
   before(:each) { log_in admin_attributes }
 
-  context '#view' do
-    it 'basic user in view page' do
+  context '#show' do
+    it 'basic user in show page' do
       visit '/users/'
       expect(current_path).to eq '/users/'
       find('.view-testing-link', visible: false).click
