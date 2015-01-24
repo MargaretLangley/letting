ac954c7b75b036bdcec2b10bd2049706bc601ed0
Richard Wigley <richard.wigley@gmail.com>
2015-01-24 16:41:03 +0000

    migration from string to symbol

--- db/migrate/20140919091254_create_invoicings.rb
+++ db/migrate/20140919091254_create_invoicings.rb
@@ -1,7 +1,7 @@
 class CreateInvoicings < ActiveRecord::Migration
   def change
     create_table :invoicings do |t|
-      t.string "property_range", null: false
+      t.string :property_range, null: false
       t.date   :period_first,      null: false
       t.date   :period_last,       null: false
       t.timestamps null: true
