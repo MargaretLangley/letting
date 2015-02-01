6d159edf34fe59f31dd9078db5081f9ec9ae80b8
Richard Wigley <richard.wigley@gmail.com>
2015-02-01 23:56:14 +0000

    Helper test now for property helper

--- spec/helpers/property_helper_spec.rb
+++ spec/helpers/property_helper_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe ClientHelper, type: :helper do
-  it '#clients - lists clients' do
+describe PropertyHelper, type: :helper do
+  it '#lists clients' do
     client_create id: 1,
                   human_ref: '8008',
                   entities: [Entity.new(title: 'Mr', name: 'Bell')]
