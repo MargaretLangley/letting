62da8f0a189ad2cdcba7e061150bcd6d9e33231d
Richard Wigley <richard.wigley@gmail.com>
2015-02-14 15:35:38 +0000

    Comment updated

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   # nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-   # Disable serving static files from the `/public` folder by default since
+  # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
