7f83f06a0e2fc86a8fe8b65b0af38ad2d8a17aff
Richard Wigley <richard.wigley@gmail.com>
2015-02-21 14:42:25 +0000

    accord => accordion

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,8 +1,8 @@
 /*jQuery time*/
 $(document).ready(function(){
-  $(".accord h3").click(function(){
+  $(".accordion h3").click(function(){
     //slide up all the link lists
-    $(".accord ul ul").slideUp();
+    $(".accordion ul ul").slideUp();
     //slide down the link list below the h3 clicked - only if its closed
     if(!$(this).next().is(":visible"))
     {
