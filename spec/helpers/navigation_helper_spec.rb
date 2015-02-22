edc544c377fc0b6a8e214e7bcfd0d920f091621a
Richard <richard.wigley@gmail.com>
2015-02-22 01:00:25 +0000

    Bugfix Navigation helper spec needed fixing

--- spec/helpers/navigation_helper_spec.rb
+++ spec/helpers/navigation_helper_spec.rb
@@ -4,11 +4,11 @@ describe NavigationHelper, type: :helper do
   describe '#main_menu_active?' do
     it 'returns "active" when "selection" is on named controller' do
       allow(controller).to receive(:controller_name).and_return('accounts')
-      expect(main_menu_active? 'accounts').to include 'active-nav'
+      expect(main_menu_active? 'accounts').to include 'active-menu'
     end
     it 'returns "inactive" when "selection" is on different controller' do
       allow(controller).to receive(:controller_name).and_return('mismatch')
-      expect(main_menu_active? 'accounts').to include 'inactive-nav'
+      expect(main_menu_active? 'accounts').to include 'inactive-menu'
     end
   end
 
