078cdf1ec24a1c7e77082d855532b712ba850b29
Richard Wigley <richard.wigley@gmail.com>
2015-03-06 11:25:36 +0000

    Revert "User interface change: Blue Invoicing => 1st Invoice, Red Invoicing => 2nd Invoice"
    This reverts commit 684f8498478a994459a4cd148d468233e4ad43fd.

--- app/decorators/run_decorator.rb
+++ app/decorators/run_decorator.rb
@@ -13,27 +13,13 @@ require_relative '../../lib/modules/method_missing'
 class RunDecorator
   include MethodMissing
   include DateHelper
-  attr_reader :index
 
   def run
     @source
   end
 
-  def initialize run, index
+  def initialize run
     @source = run
-    @index = index + 1
-  end
-
-  # Name appearing on the top of a tab
-  #
-  def tab_index_name
-    "#{index.ordinalize} Invoice"
-  end
-
-  # Outputs html id for JS
-  #
-  def tab_index_id
-    "#tab-content#{index}"
   end
 
   def style_activity
