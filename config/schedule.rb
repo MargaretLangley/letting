943d329135267b344d66a801b4432d99b943000d
Richard Wigley <richard.wigley@gmail.com>
2015-03-10 22:56:31 +0000

    Cron changes

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,13 +1,25 @@
+# file to define cron jobs
+#
 # rubocop: disable Metrics/LineLength
+#
+set :output, "#{path}/log/cron.log"
 
-set :output, '/var/log/syslog'
-job_type :rbenv_rake, %{export PATH=~/.rbenv/shims:~/.rbenv/bin:/usr/bin:$PATH; eval "$(rbenv init -)"; \
-                         cd :path && :environment_variable=:environment bundle exec rake :task --silent :output }
+case @environment
 
-job_type :rbenv_runner, %{export PATH=~/.rbenv/shims:~/.rbenv/bin:$PATH; eval "$(rbenv init -)"; \
-                         cd :path && bundle exec rails runner -e :environment ':task' :output }
+when 'development'
 
-every 1.minute do
-  rbenv_rake 'hide_monotonous_account_details'
-  rbenv_runner 'Account.hide_monotonous_account_details'
+  job_type :rbenv_rake, %{export PATH=~/.rbenv/shims:~/.rbenv/bin:/usr/bin:$PATH; eval "$(rbenv init -)"; \
+                        cd :path && :environment_variable=:environment bundle exec rake :task --silent :output }
+
+  every 1.minutes do
+    rbenv_rake 'hide_monotonous_account_details'
+  end
+
+else
+
+   job_type :rbenv_rake, %{export PATH=/opt/rbenv/shims:/opt/rbenv/bin:/usr/bin:$PATH; eval "$(rbenv init -)"; \
+                            cd :path && :environment_variable=:environment bundle exec rake :task --silent :output }
+   every 1.day, :at => '4:30 am' do
+     rbenv_rake 'hide_monotonous_account_details'
+   end
 end
--- lib/tasks/hide_monotonous_account_details.rake
+++ lib/tasks/hide_monotonous_account_details.rake
@@ -1,5 +1,4 @@
 desc 'cron task for hiding old account transactions that are balancing'
 task hide_monotonous_account_details: :environment do
-  puts 'Monotonous'
   Account.hide_monotonous_account_details
 end
