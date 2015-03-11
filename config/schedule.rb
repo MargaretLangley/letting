aa130ca20ff73264582934e2402de37f1985675c
Richard Wigley <richard.wigley@gmail.com>
2015-03-11 11:26:22 +0000

    Whenever (cron) schedule rubocop errors

--- config/schedule.rb
+++ config/schedule.rb
@@ -17,9 +17,9 @@ when 'development'
 
 else
 
-   job_type :rbenv_rake, %{export PATH=/opt/rbenv/shims:/opt/rbenv/bin:/usr/bin:$PATH; eval "$(rbenv init -)"; \
-                            cd :path && :environment_variable=:environment bundle exec rake :task --silent :output }
-   every 1.day, :at => '4:30 am' do
-     rbenv_rake 'hide_monotonous_account_details'
-   end
+  job_type :rbenv_rake, %{export PATH=/opt/rbenv/shims:/opt/rbenv/bin:/usr/bin:$PATH; eval "$(rbenv init -)"; \
+                           cd :path && :environment_variable=:environment bundle exec rake :task --silent :output }
+  every 1.day, at: '4:30 am' do
+    rbenv_rake 'hide_monotonous_account_details'
+  end
 end
