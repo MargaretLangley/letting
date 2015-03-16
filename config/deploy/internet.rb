ddf7aa813c8daed38a7fba3ffeaeb0615814fa65
Richard Wigley <richard.wigley@gmail.com>
2015-03-16 11:30:06 +0000

    Rubocop errors

--- config/deploy/internet.rb
+++ config/deploy/internet.rb
@@ -1,17 +1,17 @@
 # Production
 #
-# Capisrano environment settings
+# Capistrano environment settings
 #
 set :stage, :internet
 set :branch, 'master'
 
 # Simple Role Syntax
-role :app, %w{deployer@bcs.io}
-role :web, %w{deployer@bcs.io}
-role :db,  %w{deployer@bcs.io}
+role :app, %w(deployer@bcs.io)
+role :web, %w(deployer@bcs.io)
+role :db,  %w(deployer@bcs.io)
 
 # bcs.io
-server 'bcs.io', user: 'deployer', roles: %w{web app db}, primary: true
+server 'bcs.io', user: 'deployer', roles: %w(web app db), primary: true
 
 set :rails_env, :internet
 
